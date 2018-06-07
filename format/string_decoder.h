d9ab7e470b9eb8dfc4ec03716191beaa08a82a0f
Dustin Graves <dustin@lunarg.com>
2018-06-07 16:48:08 -0600

    Add StringDecoder
    Adds a special purpose pointer decoder class for strings.

---
+++ format/string_decoder.h
@@ -0,0 +1,62 @@
+/*
+** Copyright (c) 2018 LunarG, Inc.
+**
+** Licensed under the Apache License, Version 2.0 (the "License");
+** you may not use this file except in compliance with the License.
+** You may obtain a copy of the License at
+**
+**     http://www.apache.org/licenses/LICENSE-2.0
+**
+** Unless required by applicable law or agreed to in writing, software
+** distributed under the License is distributed on an "AS IS" BASIS,
+** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+** See the License for the specific language governing permissions and
+** limitations under the License.
+*/
+
+#ifndef BRIMSTONE_FORMAT_STRING_DECODER_H
+#define BRIMSTONE_FORMAT_STRING_DECODER_H
+
+#include <cassert>
+
+#include "util/defines.h"
+#include "format/pointer_decoder_base.h"
+
+BRIMSTONE_BEGIN_NAMESPACE(brimstone)
+BRIMSTONE_BEGIN_NAMESPACE(format)
+
+class StringDecoder : public PointerDecoderBase
+{
+public:
+    StringDecoder() : data_(nullptr), capacity_(0), is_memory_external_(false) { }
+
+    ~StringDecoder() { if ((data_ != nullptr) && !is_memory_external_) delete [] data_; }
+
+    char* GetPointer() const { return data_; }
+
+    void SetExternalMemory(char* data, size_t capacity)
+    {
+        if ((data != nullptr) && (capacity > 0))
+        {
+            data_ = data;
+            capacity_ = capacity;
+            is_memory_external_ = true;
+        }
+        else
+        {
+            // TODO: Log an error message
+        }
+    }
+
+    size_t Decode(const uint8_t* buffer, size_t buffer_size);
+
+private:
+    char*   data_;
+    size_t  capacity_;
+    bool    is_memory_external_;
+};
+
+BRIMSTONE_END_NAMESPACE(format)
+BRIMSTONE_END_NAMESPACE(brimstone)
+
+#endif // BRIMSTONE_FORMAT_STRING_DECODER_H
