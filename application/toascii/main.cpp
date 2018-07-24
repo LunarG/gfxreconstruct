d30aaafcd008b130740fd55d68d0fad4663d40de
Dustin Graves <dustin@lunarg.com>
2018-07-23 18:46:49 -0600

    Update ASCII Converter Tool with New Consumer
    Update the ASCII conversion command line utility code to use the new
    VulkanAsciiConsumer class so that it can actually perform the binary to
    ASCII conversion.  This utility had previously been partially
    implemented to test the file decoding, without performaing any specific
    actions with the decoded data.

--- application/toascii/main.cpp
+++ application/toascii/main.cpp
@@ -1,4 +1,5 @@
 #include "format/file_processor.h"
+#include "format/vulkan_ascii_consumer.h"
 #include "format/vulkan_decoder.h"
 
 int main(int argc, char** argv)
@@ -8,6 +9,10 @@ int main(int argc, char** argv)
     if (file_processor.Initialize("D:\\temp\\brimstone_test.bin"))
     {
         brimstone::format::VulkanDecoder decoder;
+        brimstone::format::VulkanAsciiConsumer ascii_consumer;
+
+        ascii_consumer.Initialize("D:\\temp\\brimstone_test.txt");
+        decoder.AddConsumer(&ascii_consumer);
 
         file_processor.AddDecoder(&decoder);
         file_processor.ProcessAllFrames();
