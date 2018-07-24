c11c6c5a5a73e3cd866623d68c2051653f0d71ea
Dustin Graves <dustin@lunarg.com>
2018-07-23 18:44:24 -0600

    Add Initial VulkanConsumer Classes
    Adds the .h and .cpp files for the VulkanConsumer abstract base class
    and VulkanAsciiConsumer class.  The Vulkan API call related method
    declarations and definitions for these classes are currently included
    from generated files.  The VulkanAsciiConsumer is intended to convert
    binary trace files to human readable ASCII files.

---
+++ format/vulkan_consumer.h
@@ -0,0 +1,46 @@
+/*
+** Copyright (c) 2018 LunarG, Inc.
+**
+** Licensed under the Apache License, Version 2.0 (the "License");
+** you may not use this file except in compliance with the License.
+** You may obtain a copy of the License at
+**
+**     http://www.apache.org/licenses/LICENSE-2.0
+**
+** Unless required by applicable law or agreed to in writing, software
+** distributed under the License is distributed on an "AS IS" BASIS,
+** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+** See the License for the specific language governing permissions and
+** limitations under the License.
+*/
+
+#ifndef BRIMSTONE_VULKAN_CONSUMER_H
+#define BRIMSTONE_VULKAN_CONSUMER_H
+
+#include <algorithm>
+#include <vector>
+
+#include "vulkan/vulkan.h"
+
+#include "util/defines.h"
+#include "format/pointer_decoder.h"
+#include "format/string_decoder.h"
+#include "format/struct_pointer_decoder.h"
+
+#include "generated/generated_struct_decoder_declarations.inc"
+
+BRIMSTONE_BEGIN_NAMESPACE(brimstone)
+BRIMSTONE_BEGIN_NAMESPACE(format)
+
+class VulkanConsumer
+{
+public:
+    virtual ~VulkanConsumer() { }
+
+#include "generated/generated_api_call_consumer_declarations.inc"
+};
+
+BRIMSTONE_END_NAMESPACE(format)
+BRIMSTONE_END_NAMESPACE(brimstone)
+
+#endif // BRIMSTONE_VULKAN_CONSUMER_H
