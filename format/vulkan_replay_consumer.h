8a94fcaab702a1364f84a2b6e42f5f00cf345ef0
Dustin Graves <dustin@lunarg.com>
2018-07-27 19:09:34 -0600

    Add VulkanReplayConsumer class
    Initial implementation of the VulkanReplayConsumer class, which includes
    the methods produced by the associated code generator.

---
+++ format/vulkan_replay_consumer.h
@@ -0,0 +1,47 @@
+/*
+** Copyright (c) 2018 LunarG, Inc.
+**
+** Licensed under the Apache License, Version 2.0 (the "License");
+** you may not use this file except in compliance with the License.
+** You may obtain a copy of the License at
+**
+**     http://www.apache.org/licenses/LICENSE-2.0
+**
+** Unless required by applicable law or agreed to in writing, software
+** distributed under the License is distributed on an "AS IS" BASIS,
+** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+** See the License for the specific language governing permissions and
+** limitations under the License.
+*/
+
+#ifndef BRIMSTONE_VULKAN_REPLAY_CONSUMER_H
+#define BRIMSTONE_VULKAN_REPLAY_CONSUMER_H
+
+#include <cstdio>
+#include <string>
+
+#include "vulkan/vulkan.h"
+
+#include "util/defines.h"
+#include "format/vulkan_consumer.h"
+
+BRIMSTONE_BEGIN_NAMESPACE(brimstone)
+BRIMSTONE_BEGIN_NAMESPACE(format)
+
+class VulkanReplayConsumer : public VulkanConsumer
+{
+public:
+    VulkanReplayConsumer();
+
+    virtual ~VulkanReplayConsumer();
+
+    const VkAllocationCallbacks*
+    GetAllocationCallbacks(const StructPointerDecoder<Decoded_VkAllocationCallbacks>& original_callbacks);
+
+#include "generated/generated_api_call_consumer_override_declarations.inc"
+};
+
+BRIMSTONE_END_NAMESPACE(format)
+BRIMSTONE_END_NAMESPACE(brimstone)
+
+#endif // BRIMSTONE_VULKAN_REPLAY_CONSUMER_H
