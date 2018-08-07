fdbb51d63ae8389f83c8ea18162b28d9df8067fb
Dustin Graves <dustin@lunarg.com>
2018-08-06 20:12:12 -0600

    Add logging/error handling to replay tool
    - Initialize logging system.
    - Register a fatal error handler with the replay consumer, which throws
      a std::runtime_error.
    - Add an exception handler around frame processing to catch any fatal
      errors that may occur during playback.

--- application/replay/main.cpp
+++ application/replay/main.cpp
@@ -1,21 +1,42 @@
+#include <cstdio>
+#include <exception>
+
+#include "util/logging.h"
 #include "format/file_processor.h"
 #include "format/vulkan_replay_consumer.h"
 #include "format/vulkan_decoder.h"
 
 int main(int argc, char** argv)
 {
+    brimstone::util::logging::Init();
+
     brimstone::format::FileProcessor file_processor;
 
     if (file_processor.Initialize("D:\\temp\\brimstone_test.bin"))
     {
-        brimstone::format::VulkanDecoder decoder;
-        brimstone::format::VulkanReplayConsumer ascii_consumer;
+        brimstone::format::VulkanDecoder        decoder;
+        brimstone::format::VulkanReplayConsumer replay_consumer;
 
-        decoder.AddConsumer(&ascii_consumer);
+        replay_consumer.SetFatalErrorHandler([](const char* message) { throw std::runtime_error(message); });
 
+        decoder.AddConsumer(&replay_consumer);
         file_processor.AddDecoder(&decoder);
-        file_processor.ProcessAllFrames();
+
+        try
+        {
+            file_processor.ProcessAllFrames();
+        }
+        catch (std::runtime_error error)
+        {
+            printf("Replay failed with error message: %s\n", error.what());
+        }
+        catch (...)
+        {
+            printf("Replay failed due to an unhandled exception\n");
+        }
     }
+
+    brimstone::util::logging::Release();
 
     return 0;
 }
