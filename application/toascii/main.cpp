7d6089ddb58263ecf119305bb8608a17b4eb55c6
Mark Young <marky@lunarg.com>
2018-08-07 10:36:17 -0600

    applications: Modify to take binary as input
    Modify the application executables to take in the brimstone binary
    as the only command-line argument.  This lets me work on this
    without having to hard-code any path.

--- application/toascii/main.cpp
+++ application/toascii/main.cpp
@@ -5,13 +5,29 @@
 int main(int argc, char** argv)
 {
     brimstone::format::FileProcessor file_processor;
+    std::string bin_file_name = "D:\\temp\\brimstone_test.bin";
 
-    if (file_processor.Initialize("D:\\temp\\brimstone_test.bin"))
+    if (argc > 1)
+    {
+        bin_file_name = argv[1];
+    }
+    std::string text_file_name = bin_file_name;
+    size_t suffix_pos = text_file_name.find(".bin");
+    if (suffix_pos == std::string::npos)
+    {
+        text_file_name += ".txt";
+    }
+    else
+    {
+        text_file_name.replace(suffix_pos, 4, ".txt");
+    }
+
+    if (file_processor.Initialize(bin_file_name))
     {
         brimstone::format::VulkanDecoder decoder;
         brimstone::format::VulkanAsciiConsumer ascii_consumer;
 
-        ascii_consumer.Initialize("D:\\temp\\brimstone_test.txt");
+        ascii_consumer.Initialize(text_file_name);
         decoder.AddConsumer(&ascii_consumer);
 
         file_processor.AddDecoder(&decoder);
