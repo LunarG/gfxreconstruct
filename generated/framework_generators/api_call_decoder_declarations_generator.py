5badbff863776657d66e160d603aee605194232c
Dustin Graves <dustin@lunarg.com>
2018-08-15 18:51:28 -0600

    Add new generators for decode declarations
    Re-implement StructDecoderDeclarationsGenerator,
    ApiCallDecoderDeclarationsGenerator, and ApiCallDecodeCasesGenerator on
    top of the new BaseGenerator class

---
+++ generated/framework_generators/api_call_decoder_declarations_generator.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3 -i
+#
+# Copyright (c) 2018 LunarG, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os,re,sys
+from base_generator import *
+
+class ApiCallDecoderDeclarationsGeneratorOptions(BaseGeneratorOptions):
+    """Options for Vulkan API parameter decoding C++ code generation"""
+    def __init__(self,
+                 blacklists = None,         # Path to JSON file listing apicalls and structs to ignore.
+                 platformTypes = None,      # Path to JSON file listing platform (WIN32, X11, etc.) defined types.
+                 filename = None,
+                 directory = '.',
+                 prefixText = '',
+                 protectFile = False,
+                 protectFeature = True):
+        BaseGeneratorOptions.__init__(self, blacklists, platformTypes,
+                                      filename, directory, prefixText,
+                                      protectFile, protectFeature)
+
+# ApiCallDecoderDeclarationsGenerator - subclass of BaseGenerator.
+# Generates C++ member declarations for the VulkanDecoder class responsible for decoding
+# Vulkan API call parameter data.
+class ApiCallDecoderDeclarationsGenerator(BaseGenerator):
+    """Generate API parameter decoding C++ member declarations"""
+    def __init__(self,
+                 errFile = sys.stderr,
+                 warnFile = sys.stderr,
+                 diagFile = sys.stdout):
+        BaseGenerator.__init__(self, errFile, warnFile, diagFile)
+
+    #
+    # Indicates that the current feature has C++ code to generate.
+    def needFeatureGeneration(self):
+        if self.featureCmdParams:
+            return True
+        return False
+
+    #
+    # Performs C++ code generation for the feature.
+    def generateFeature(self):
+        first = True
+        for cmd in self.featureCmdParams:
+            cmddef = '' if first else '\n'
+            cmddef += '    size_t Decode_{}(const uint8_t* parameter_buffer, size_t buffer_size);'.format(cmd)
+            write(cmddef, file=self.outFile)
+            first = False
