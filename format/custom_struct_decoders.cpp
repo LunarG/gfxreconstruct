96d707c05a75676e557b3b1663aa67f80e426640
Dustin Graves <dustin@lunarg.com>
2018-08-21 16:22:52 -0600

    Implement SECURITY_ATTRIBUTES decoder
    Implements a custom decoder for the WIN32 SECURITY_ATTRIBUTES structure
    used with some WIN32 platform extensions.
    - Add a new file to define platform specific types for other platforms.
    - Add value decoders for uint8 and uint16, which appear in the
      SECURITY_ATTRIBUTES struct.
    - Add Decoded struct wrappers for SECURITY_ATTRIBUTES and associated
      structures.
    - Add decode_struct() implementations for new struct wrappers.

---
+++ format/custom_struct_decoders.cpp
@@ -0,0 +1,117 @@
+/*
+** Copyright (c) 2018 LunarG, Inc.
+**
+** Licensed under the Apache License, Version 2.0 (the "License");
+** you may not use this file except in compliance with the License.
+** You may obtain a copy of the License at
+**
+**     http://www.apache.org/licenses/LICENSE-2.0
+**
+** Unless required by applicable law or agreed to in writing, software
+** distributed under the License is distributed on an "AS IS" BASIS,
+** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+** See the License for the specific language governing permissions and
+** limitations under the License.
+*/
+
+#include <cassert>
+
+#include "format/custom_struct_decoders.h"
+#include "format/value_decoder.h"
+#include "util/defines.h"
+
+BRIMSTONE_BEGIN_NAMESPACE(brimstone)
+BRIMSTONE_BEGIN_NAMESPACE(format)
+
+// The WIN32 SID structure has a variable size, so was encoded as an array of bytes instead of a struct.
+static std::unique_ptr<uint8_t[]> unpack_sid_struct(const PointerDecoder<uint8_t>& packed_value)
+{
+    const uint8_t* bytes = packed_value.GetPointer();
+
+    // Allocate memory for variable length SID struct, to use for unpacking.
+    // SidAuthorityCount is the second byte of the packed array.
+    size_t                     sid_authority_size = bytes[1] * sizeof(uint32_t);
+
+    // sizeof(SID) already includes the size of one of the SidAuthority elements,
+    // so we can subtract 4 bytes from sid_authority_size.
+    size_t                     allocation_size = sizeof(SID) + (sid_authority_size - sizeof(uint32_t));
+    std::unique_ptr<uint8_t[]> unpacked_memory = std::make_unique<uint8_t[]>(allocation_size);
+
+    SID* sid               = reinterpret_cast<SID*>(unpacked_memory.get());
+    sid->Revision          = bytes[0];
+    sid->SubAuthorityCount = bytes[1];
+    memcpy(sid->IdentifierAuthority.Value, &bytes[2], 6);
+    memcpy(sid->SubAuthority, &bytes[8], sid_authority_size);
+
+    return unpacked_memory;
+}
+
+size_t decode_struct(const uint8_t* buffer, size_t buffer_size, Decoded_ACL* wrapper)
+{
+    assert((wrapper != nullptr) && (wrapper->value != nullptr));
+
+    size_t bytes_read = 0;
+    ACL*   value      = wrapper->value;
+
+    bytes_read +=
+        ValueDecoder::DecodeUInt8Value((buffer + bytes_read), (buffer_size - bytes_read), &(value->AclRevision));
+    bytes_read += ValueDecoder::DecodeUInt8Value((buffer + bytes_read), (buffer_size - bytes_read), &(value->Sbz1));
+    bytes_read += ValueDecoder::DecodeUInt16Value((buffer + bytes_read), (buffer_size - bytes_read), &(value->AclSize));
+    bytes_read +=
+        ValueDecoder::DecodeUInt16Value((buffer + bytes_read), (buffer_size - bytes_read), &(value->AceCount));
+    bytes_read += ValueDecoder::DecodeUInt16Value((buffer + bytes_read), (buffer_size - bytes_read), &(value->Sbz2));
+
+    return bytes_read;
+}
+
+size_t decode_struct(const uint8_t* buffer, size_t buffer_size, Decoded_SECURITY_DESCRIPTOR* wrapper)
+{
+    assert((wrapper != nullptr) && (wrapper->value != nullptr));
+
+    size_t               bytes_read = 0;
+    SECURITY_DESCRIPTOR* value      = wrapper->value;
+
+    bytes_read += ValueDecoder::DecodeUInt8Value((buffer + bytes_read), (buffer_size - bytes_read), &(value->Revision));
+    bytes_read += ValueDecoder::DecodeUInt8Value((buffer + bytes_read), (buffer_size - bytes_read), &(value->Sbz1));
+    bytes_read += ValueDecoder::DecodeUInt16Value((buffer + bytes_read), (buffer_size - bytes_read), &(value->Control));
+
+    // The SID structure has a variable size, so has been packed into an array of bytes.
+    bytes_read += wrapper->PackedOwner.DecodeUInt8((buffer + bytes_read), (buffer_size - bytes_read));
+    wrapper->Owner = unpack_sid_struct(wrapper->PackedOwner);
+    value->Owner   = wrapper->Owner.get();
+
+    bytes_read += wrapper->PackedGroup.DecodeUInt8((buffer + bytes_read), (buffer_size - bytes_read));
+    wrapper->Group = unpack_sid_struct(wrapper->PackedOwner);
+    value->Group   = wrapper->Group.get();
+
+    bytes_read += wrapper->Sacl.Decode((buffer + bytes_read), (buffer_size - bytes_read));
+    value->Sacl = wrapper->Sacl.GetPointer();
+
+    bytes_read += wrapper->Dacl.Decode((buffer + bytes_read), (buffer_size - bytes_read));
+    value->Dacl = wrapper->Dacl.GetPointer();
+
+    return bytes_read;
+}
+
+size_t decode_struct(const uint8_t* buffer, size_t buffer_size, Decoded_SECURITY_ATTRIBUTES* wrapper)
+{
+    assert((wrapper != nullptr) && (wrapper->value != nullptr));
+
+    size_t               bytes_read = 0;
+    SECURITY_ATTRIBUTES* value      = wrapper->value;
+
+    uint32_t nLength = 0;
+    bytes_read += ValueDecoder::DecodeUInt32Value((buffer + bytes_read), (buffer_size - bytes_read), &nLength);
+    value->nLength = nLength;
+
+    bytes_read += wrapper->lpSecurityDescriptor.Decode((buffer + bytes_read), (buffer_size - bytes_read));
+    value->lpSecurityDescriptor = wrapper->lpSecurityDescriptor.GetPointer();
+
+    bytes_read +=
+        ValueDecoder::DecodeInt32Value((buffer + bytes_read), (buffer_size - bytes_read), &(value->bInheritHandle));
+
+    return bytes_read;
+}
+
+BRIMSTONE_END_NAMESPACE(format)
+BRIMSTONE_END_NAMESPACE(brimstone)
---
+++ format/custom_struct_decoders_forward.h
@@ -0,0 +1,38 @@
+/*
+** Copyright (c) 2018 LunarG, Inc.
+**
+** Licensed under the Apache License, Version 2.0 (the "License");
+** you may not use this file except in compliance with the License.
+** You may obtain a copy of the License at
+**
+**     http://www.apache.org/licenses/LICENSE-2.0
+**
+** Unless required by applicable law or agreed to in writing, software
+** distributed under the License is distributed on an "AS IS" BASIS,
+** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+** See the License for the specific language governing permissions and
+** limitations under the License.
+*/
+
+#ifndef BRIMSTONE_CUSTOM_STRUCT_DECODER_DECLARATIONS_H
+#define BRIMSTONE_CUSTOM_STRUCT_DECODER_DECLARATIONS_H
+
+#include "format/platform_types.h"
+#include "util/defines.h"
+
+BRIMSTONE_BEGIN_NAMESPACE(brimstone)
+BRIMSTONE_BEGIN_NAMESPACE(format)
+
+// Decoded struct wrappers for SECURITY_ATTRIBUTES and related WIN32 structures.
+struct Decoded_ACL;
+struct Decoded_SECURITY_DESCRIPTOR;
+struct Decoded_SECURITY_ATTRIBUTES;
+
+size_t decode_struct(const uint8_t* buffer, size_t buffer_size, Decoded_ACL* wrapper);
+size_t decode_struct(const uint8_t* buffer, size_t buffer_size, Decoded_SECURITY_DESCRIPTOR* wrapper);
+size_t decode_struct(const uint8_t* buffer, size_t buffer_size, Decoded_SECURITY_ATTRIBUTES* wrapper);
+
+BRIMSTONE_END_NAMESPACE(format)
+BRIMSTONE_END_NAMESPACE(brimstone)
+
+#endif // BRIMSTONE_CUSTOM_STRUCT_DECODER_DECLARATIONS_H
