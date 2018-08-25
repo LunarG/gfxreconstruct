8e54996f296e5a7ec774fe778ba3cd36ab656a8e
Dustin Graves <dustin@lunarg.com>
2018-08-24 19:23:24 -0600

    Update replay tool's exit codes
    Update grecreplay to return a non-zero exit code for all failure cases.

--- application/replay/main.cpp
+++ application/replay/main.cpp
@@ -43,6 +43,8 @@ void PrintUsage(const char* exe_name)
 
 int main(int argc, const char** argv)
 {
+    int return_code = 0;
+
     brimstone::util::logging::Init();
 
     brimstone::format::FileProcessor file_processor;
@@ -53,68 +55,78 @@ int main(int argc, const char** argv)
     if (arg_parser.IsInvalid() || non_optional_arguments.size() != 1)
     {
         PrintUsage(argv[0]);
-        exit(-1);
+        return_code = -1;
     }
     else
     {
         bin_file_name = non_optional_arguments[0];
-    }
-
-    if (file_processor.Initialize(bin_file_name))
-    {
-        // Setup platform specific application and window factory.
-        std::unique_ptr<brimstone::application::Application> application;
-        std::unique_ptr<brimstone::format::WindowFactory>    window_factory;
-
-#if defined(WIN32)
-#if defined(VK_USE_PLATFORM_WIN32_KHR)
-        brimstone::application::Win32Application* win32_application = new brimstone::application::Win32Application();
-        application    = std::unique_ptr<brimstone::application::Application>(win32_application);
-        window_factory = std::make_unique<brimstone::application::Win32WindowFactory>(win32_application);
-#else
-#error "Missing support for window management (need to define VK_USE_PLATFORM_WIN32_KHR)"
-#endif
-#else
-#if defined(VK_USE_PLATFORM_XCB_KHR)
-        brimstone::application::XcbApplication* xcb_application = new brimstone::application::XcbApplication();
-        application    = std::unique_ptr<brimstone::application::Application>(xcb_application);
-        window_factory = std::make_unique<brimstone::application::XcbWindowFactory>(xcb_application);
-#endif
-#if defined(VK_USE_PLATFORM_WAYLAND_KHR)
-        brimstone::application::WaylandApplication* wayland_application =
-            new brimstone::application::WaylandApplication();
-        application    = std::unique_ptr<brimstone::application::Application>(wayland_application);
-        window_factory = std::make_unique<brimstone::application::WaylandWindowFactory>(wayland_application);
-#else
-#error "Missing support for window management (need to define VK_USE_PLATFORM_XCB_KHR or VK_USE_PLATFORM_WAYLAND_KHR)"
-#endif
-#endif
-
-        application->SetFileProcessor(&file_processor);
-
-        brimstone::format::VulkanDecoder    decoder;
-        brimstone::format::VulkanReplayConsumer replay_consumer(window_factory.get());
-
-        replay_consumer.SetFatalErrorHandler([](const char* message) { throw std::runtime_error(message); });
-
-        decoder.AddConsumer(&replay_consumer);
-        file_processor.AddDecoder(&decoder);
 
         try
         {
-            application->Run();
+            std::unique_ptr<brimstone::application::Application> application;
+            std::unique_ptr<brimstone::format::WindowFactory>    window_factory;
+
+            if (!file_processor.Initialize(bin_file_name))
+            {
+                printf("Failed to load file %s.\n", bin_file_name.c_str());
+                return_code = -1;
+            }
+            else
+            {
+                // Setup platform specific application and window factory.
+#if defined(WIN32)
+#if defined(VK_USE_PLATFORM_WIN32_KHR)
+                brimstone::application::Win32Application* win32_application = new brimstone::application::Win32Application();
+                application    = std::unique_ptr<brimstone::application::Application>(win32_application);
+                window_factory = std::make_unique<brimstone::application::Win32WindowFactory>(win32_application);
+#endif
+#if defined(VK_USE_PLATFORM_XCB_KHR)
+                brimstone::application::XcbApplication* xcb_application = new brimstone::application::XcbApplication();
+                application    = std::unique_ptr<brimstone::application::Application>(xcb_application);
+                window_factory = std::make_unique<brimstone::application::XcbWindowFactory>(xcb_application);
+#elif defined(VK_USE_PLATFORM_WAYLAND_KHR)
+                brimstone::application::WaylandApplication* wayland_application =
+                    new brimstone::application::WaylandApplication();
+                application    = std::unique_ptr<brimstone::application::Application>(wayland_application);
+                window_factory = std::make_unique<brimstone::application::WaylandWindowFactory>(wayland_application);
+#endif
+#endif
+            }
+
+            if (!application || !window_factory)
+            {
+                printf("Failed to initialize platform sepcific window system management.\nEnsure that the appropriate "
+                       "Vulkan platform extensions have been eneabled.\n");
+                return_code = -1;
+            }
+            else
+            {
+                application->SetFileProcessor(&file_processor);
+
+                brimstone::format::VulkanDecoder    decoder;
+                brimstone::format::VulkanReplayConsumer replay_consumer(window_factory.get());
+
+                replay_consumer.SetFatalErrorHandler([](const char* message) { throw std::runtime_error(message); });
+
+                decoder.AddConsumer(&replay_consumer);
+                file_processor.AddDecoder(&decoder);
+
+                application->Run();
+            }
         }
         catch (std::runtime_error error)
         {
             printf("Replay failed with error message: %s\n", error.what());
+            return_code = -1;
         }
         catch (...)
         {
             printf("Replay failed due to an unhandled exception\n");
+            return_code = -1;
         }
     }
 
     brimstone::util::logging::Release();
 
-    return 0;
+    return return_code;
 }
