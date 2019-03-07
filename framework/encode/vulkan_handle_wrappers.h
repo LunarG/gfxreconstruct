3eedd0f6676b2dd23f7f9abd3383800a70ec35a7
Dustin Graves <dustin@lunarg.com>
2019-03-06 17:06:30 -0700

    Add handle wrappers
    Add wrappers for vulkan handles, which will contain the following:
    - The handle returned by the driver.
    - A unique ID number generated by the capture layer.
    - State tracking information required for trimming.
    
    Change-Id: I1b9b9bc9781744400873aa8581ed7fe723108345

---
+++ framework/encode/vulkan_handle_wrappers.h
@@ -0,0 +1,260 @@
+/*
+** Copyright (c) 2019 LunarG, Inc.
+**
+** Licensed under the Apache License, Version 2.0 (the "License");
+** you may not use this file except in compliance with the License.
+** You may obtain a copy of the License at
+**
+**     http://www.apache.org/licenses/LICENSE-2.0
+**
+** Unless required by applicable law or agreed to in writing, software
+** distributed under the License is distributed on an "AS IS" BASIS,
+** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+** See the License for the specific language governing permissions and
+** limitations under the License.
+*/
+
+#ifndef GFXRECON_ENCODE_HANDLE_WRAPPERS_H
+#define GFXRECON_ENCODE_HANDLE_WRAPPERS_H
+
+#include "format/format.h"
+#include "util/defines.h"
+#include "util/memory_output_stream.h"
+
+#include "vulkan/vulkan.h"
+
+#include <limits>
+#include <memory>
+#include <unordered_map>
+#include <vector>
+
+GFXRECON_BEGIN_NAMESPACE(gfxrecon)
+GFXRECON_BEGIN_NAMESPACE(encode)
+
+//
+// Types for state tracking.
+//
+
+typedef std::shared_ptr<util::MemoryOutputStream> CreateParameters;
+
+// Active query state information stored with VkQueryPool handle.
+struct QueryInfo
+{
+    static const uint32_t kInvalidIndex = std::numeric_limits<uint32_t>::max();
+
+    VkQueryControlFlags flags{ 0 };
+    uint32_t            index{ kInvalidIndex };           // Pool index for active query.
+    VkCommandBuffer     command_buffer{ VK_NULL_HANDLE }; // Command buffer for query begin.
+    format::HandleId    command_buffer_id{ 0 };
+    VkRenderPass        render_pass{ VK_NULL_HANDLE }; // Optional render pass containing query.
+    format::HandleId    render_pass_id{ 0 };
+};
+
+// VkDescriptorSetLayout create info stored with VkPipelineLayout handle.
+struct DescriptorSetLayoutInfo
+{
+    VkDescriptorSetLayout handle{ VK_NULL_HANDLE };
+    format::HandleId      handle_id{ 0 };
+    format::ApiCallId     create_call_id{ format::ApiCallId::ApiCall_Unknown };
+    CreateParameters      create_parameters;
+};
+
+// Create info for all descriptor set layouts required by a pipeline layout.
+// Referenced with a shared pointer by VkPipelineLayout and VkPipeline handles.
+struct PipelineLayoutDependencies
+{
+    std::vector<DescriptorSetLayoutInfo> layouts;
+};
+
+// VkShaderModule create info stored with VkPipeline handle.
+struct ShaderModuleInfo
+{
+    VkShaderModule    handle{ VK_NULL_HANDLE };
+    format::HandleId  handle_id{ 0 };
+    format::ApiCallId create_call_id{ format::ApiCallId::ApiCall_Unknown };
+    CreateParameters  create_parameters;
+};
+
+//
+// Handle wrappers for storing object state information with object handles.
+//
+
+template <typename T>
+struct HandleWrapper
+{
+    typedef T HandleType;
+
+    // Standard state info required for all handles.
+    T                 handle{ VK_NULL_HANDLE };
+    format::HandleId  handle_id{ 0 };
+    format::ApiCallId create_call_id{ format::ApiCallId::ApiCall_Unknown };
+    CreateParameters  create_parameters;
+};
+
+//
+// Type definitions for handle wrappers that do not require additional state info.
+//
+
+// clang-format off
+struct QueueWrapper                     : public HandleWrapper<VkQueue> {};
+struct FenceWrapper                     : public HandleWrapper<VkFence> {};
+struct BufferWrapper                    : public HandleWrapper<VkBuffer> {};
+struct ImageWrapper                     : public HandleWrapper<VkImage> {};
+struct EventWrapper                     : public HandleWrapper<VkEvent> {};
+struct BufferViewWrapper                : public HandleWrapper<VkBufferView> {};
+struct ImageViewWrapper                 : public HandleWrapper<VkImageView> {};
+struct ShaderModuleWrapper              : public HandleWrapper<VkShaderModule> {};
+struct PipelineCacheWrapper             : public HandleWrapper<VkPipelineCache> {};
+struct DescriptorSetLayoutWrapper       : public HandleWrapper<VkDescriptorSetLayout> {};
+struct SamplerWrapper                   : public HandleWrapper<VkSampler> {};
+struct FramebufferWrapper               : public HandleWrapper<VkFramebuffer> {};
+struct SamplerYcbcrConversionWrapper    : public HandleWrapper<VkSamplerYcbcrConversion> {};
+struct DescriptorUpdateTemplateWrapper  : public HandleWrapper<VkDescriptorUpdateTemplate> {};
+struct SurfaceKHRWrapper                : public HandleWrapper<VkSurfaceKHR> {};
+struct DebugReportCallbackEXTWrapper    : public HandleWrapper<VkDebugReportCallbackEXT> {};
+struct DebugUtilsMessengerEXTWrapper    : public HandleWrapper<VkDebugUtilsMessengerEXT> {};
+struct ValidationCacheEXTWrapper        : public HandleWrapper<VkValidationCacheEXT> {};
+struct IndirectCommandsLayoutNVXWrapper : public HandleWrapper<VkIndirectCommandsLayoutNVX> {};
+
+// This handle type has a create function, but no destroy function. The handle wrapper will be owned by the VkDisplay
+// handle wrapper, which will ensure it is destroyed when the VkDisplay handle wrapper is destroyed.
+struct DisplayModeKHRWrapper            : public HandleWrapper<VkDisplayModeKHR> {};
+// clang-format on
+
+// Handle alias types for extension handle types that have been promoted to core types.
+typedef VkSamplerYcbcrConversionKHR   SamplerYcbcrConversionKHRWrapper;
+typedef VkDescriptorUpdateTemplateKHR DescriptorUpdateTemplateKHRWrapper;
+
+//
+// Declarations for handle wrappers that require additional state info.
+//
+
+// This handle type is retrieved and has no destroy function. The handle wrapper will be owned by the VkPhysicalDevice
+// handle wrapper, which will ensure it is destroyed when the VkPhysicalDevice handle wrapper is destroyed.
+struct DisplayKHRWrapper : public HandleWrapper<VkDisplayKHR>
+{
+    std::vector<DisplayModeKHRWrapper*> display_modes;
+};
+
+// This handle type is retrieved and has no destroy function. The handle wrapper will be owned by the VkInstance
+// handle wrapper, which will ensure it is destroyed when the VkInstance handle wrapper is destroyed.
+struct PhysicalDeviceWrapper : public HandleWrapper<VkPhysicalDevice>
+{
+    std::unordered_map<VkDisplayKHR, DisplayKHRWrapper*> displays;
+};
+
+struct InstanceWrapper : public HandleWrapper<VkInstance>
+{
+    std::unordered_map<VkPhysicalDevice, PhysicalDeviceWrapper*> physical_devices;
+};
+
+struct DeviceWrapper : public HandleWrapper<VkDevice>
+{
+    std::unordered_map<VkQueue, QueueWrapper*> queues;
+};
+
+struct SemaphoreWrapper : public HandleWrapper<VkSemaphore>
+{
+    // Track signal/wait: when submitted for signal, set a bit in a bitmask to identify signal source (queue submit,
+    // acquire next image, etc). On wait, clear bitmask.  On state write, if mask is not 0, submit a signal operation.
+};
+
+struct CommandPoolWrapper;
+struct CommandBufferWrapper : public HandleWrapper<VkCommandBuffer>
+{
+    // TODO: Track recording state.
+
+    // Pool from which command buffer was allocated. The command buffer must be removed from the pool's allocation list
+    // when destroyed.
+    CommandPoolWrapper* pool{ nullptr };
+};
+
+struct DeviceMemoryWrapper : public HandleWrapper<VkDeviceMemory>
+{
+    // TODO: Track mapped state
+};
+
+struct QueryPoolWrapper : public HandleWrapper<VkQueryPool>
+{
+    // TODO: Check on 'vkCmdWriteTimestamp` requirements: do we track if timestamp was or was not written for active
+    // queries, and then write timestamp with state.
+
+    // vkCmdBeginQuery or vkCmdBeginQueryIndexedEXT parameters for active queries: query index to query flags and type
+    // specific index (transform feedback vertex stream index).
+    std::unordered_map<uint32_t, QueryInfo> active_queries;
+};
+
+struct PipelineLayoutWrapper : public HandleWrapper<VkPipelineLayout>
+{
+    // Creation info for objects used to create the pipeline layout, which may have been destroyed after pipeline layout
+    // creation.
+    std::shared_ptr<PipelineLayoutDependencies> layout_dependencies;
+};
+
+struct RenderPassWrapper : public HandleWrapper<VkRenderPass>
+{
+    // TODO: track begin end render pass and subpass states
+};
+
+struct PipelineWrapper : public HandleWrapper<VkPipeline>
+{
+    // Creation info for objects used to create the pipeline, which may have been destroyed after pipeline creation.
+    std::vector<ShaderModuleInfo> shader_modules;
+
+    // TODO: This only requires the unique sequence number once handles are fully wrapped.
+    VkRenderPass      render_pass{ VK_NULL_HANDLE };
+    format::HandleId  render_pass_id{ 0 };
+    format::ApiCallId render_pass_create_call_id{ format::ApiCallId::ApiCall_Unknown };
+    CreateParameters  render_pass_create_parameters;
+
+    // TODO: This only requires the unique sequence number once handles are fully wrapped.
+    VkPipelineLayout                            layout{ VK_NULL_HANDLE };
+    format::HandleId                            layout_id{ 0 };
+    format::ApiCallId                           layout_create_call_id{ format::ApiCallId::ApiCall_Unknown };
+    CreateParameters                            layout_create_parameters;
+    std::shared_ptr<PipelineLayoutDependencies> layout_dependencies;
+
+    // TODO: Base pipeline
+    // TODO: Pipeline cache
+};
+
+struct DescriptorPoolWrapper;
+struct DescriptorSetWrapper : public HandleWrapper<VkDescriptorSet>
+{
+    // Pool from which set was allocated. The set must be removed from the pool's allocation list when destroyed.
+    DescriptorPoolWrapper* pool{ nullptr };
+
+    // TODO: Track descriptor write state.
+};
+
+struct DescriptorPoolWrapper : public HandleWrapper<VkDescriptorPool>
+{
+    // Track descriptor set info, which must be destroyed on descriptor pool reset.
+    std::unordered_map<VkDescriptorSet, DescriptorSetWrapper*> allocated_sets;
+};
+
+struct CommandPoolWrapper : public HandleWrapper<VkCommandPool>
+{
+    // Track command buffer info, which must be destroyed on command pool reset.
+    std::unordered_map<VkCommandBuffer, CommandBufferWrapper*> allocated_buffers;
+};
+
+struct SwapchainKHRWrapper : public HandleWrapper<VkSwapchainKHR>
+{
+    std::vector<bool> image_needs_acquire;
+};
+
+struct ObjectTableNVXWrapper : public HandleWrapper<VkObjectTableNVX>
+{
+    // TODO: Determine what additional state tracking is needed.
+};
+
+struct AccelerationStructureNVWrapper : public HandleWrapper<VkAccelerationStructureNV>
+{
+    // TODO: Determine what additional state tracking is needed.
+};
+
+GFXRECON_END_NAMESPACE(encode)
+GFXRECON_END_NAMESPACE(gfxrecon)
+
+#endif
