13e22997e132de5703c6ff9877090a2ac7b274de
Dror Smolarsky <Dror.Smolarsky@amd.com>
2019-05-01 16:22:44 -0400

    Add a pre build target for each project to apply C++ code style
    * Add apply code style cmake option
    * Add apply C++ code style macro that handles applying code style for a single taget.
    * Change all non android project targets to apply the code style
    * Use clang-format to apply the code style
    
    VKTRACE-115
    
    Change-Id: I8637869dbc7165094b7df7ea406dde989e9c2430

---
+++ cmake/CodeStyle.cmake
@@ -0,0 +1,41 @@
+###############################################################################
+# Copyright (c) 2019 Advanced Micro Devices, Inc. All rights reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Author: AMD Developer Tools Team
+# Description: CMake code stle build directives
+###############################################################################
+
+cmake_minimum_required(VERSION 3.1)
+
+option(APPLY_CPP_CODE_STYLE "Apply C++ code style using clang format" ON)
+
+find_program(CLANG_FORMAT clang-format DOC "Clang format executable")
+
+# Apply code style build directives
+macro(cpp_code_style_build_directives TARGET)
+    if(${APPLY_CPP_CODE_STYLE})
+        if(CLANG_FORMAT-NOTFOUND STREQUAL ${CLANG_FORMAT})
+            message(FATAL_ERROR "Failed to find clang-format in system path")
+        endif()
+        get_target_property(TARGET_SOURCE_FILES ${TARGET} SOURCES)
+        list(FILTER TARGET_SOURCE_FILES EXCLUDE REGEX ".+\.def")
+        add_custom_target("${TARGET}ClangFormat"
+                COMMAND ${CLANG_FORMAT} -i ${TARGET_SOURCE_FILES}
+                WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
+                COMMENT "Run clang format for ${TARGET}"
+                COMMAND_EXPAND_LISTS)
+        add_dependencies(${TARGET} "${TARGET}ClangFormat")
+    endif()
+endmacro()
