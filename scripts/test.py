87b7750294df4941ddc726859a82d63f62999624
Dror Smolarsky <Dror.Smolarsky@amd.com>
2019-08-09 10:53:22 -0400

    Address review comments for test script.
    * Remove unused import
    * Imporve script argument help messages
    * Remove some unneeded default function arguments
    * Fix an issue when passing a test executable using relative paths
    * Add build.py to the test package - the test script uses some definitions
      from the build script.
    
    Change-Id: Ib235eb5e71af225ea0805a62e13db46fb150a583

--- scripts/test.py
+++ scripts/test.py
@@ -18,6 +18,8 @@ GFX reconstruct build script
 '''
 
 import argparse
+import collections
+import copy
 import importlib
 import os
 import platform
@@ -32,16 +34,19 @@ def is_windows():
     return 'windows' == platform.system().lower()
 
 
-BUILD_ROOT = os.path.abspath(os.path.join(
+# Repository root directory
+REPO_ROOT = os.path.abspath(os.path.join(
     os.path.split(os.path.abspath(__file__))[0], '..'))
-ALL_TESTS = {
+
+# List of tests to run when no test executable is passed to script
+ALL_TESTS = collections.OrderedDict({
     'gfxrecon_application_test': [],
     'gfxrecon_decode_test': [],
     'gfxrecon_encode_test': [],
     'gfxrecon_format_test': [],
     'gfxrecon_util_test': [],
     'VkLayer_gfxreconstruct_test': [],
-}
+})
 
 
 class TestError(Exception):
@@ -54,7 +59,7 @@ def import_build_script():
     '''
     Import the build script - reuse build script constants
     '''
-    sys.path.insert(0, os.path.join(BUILD_ROOT, 'scripts'))
+    sys.path.insert(0, os.path.join(REPO_ROOT, 'scripts'))
     build_script = importlib.import_module('build')
     return build_script
 
@@ -65,14 +70,16 @@ def parse_args(build_script):
     '''
     arg_parser = argparse.ArgumentParser(
         description="gfxreconstruct test script")
-    arg_parser.add_argument('--version', dest='version',
-                            action='version', version=str(build_script.VERSION))
+    arg_parser.add_argument('--version', action='version',
+                            version=str(build_script.VERSION),
+                            help='Print script version and exit')
     arg_parser.add_argument(
-        '--test-exe', metavar='TEST_EXE', action='store', default=None,
-        help='Test executable to run')
+        '--test-exe', default=None,
+        help='the name of a single internal test to run (one of: {}), or a '
+        'path to an external test to run.  If not specified, all internal '
+        'tests are run.'.format(', '.join(ALL_TESTS)))
     arg_parser.add_argument(
-        '--test-args', dest='test_args', metavar='TEST_ARGS', nargs='+',
-        action='store',
+        '--test-args', nargs='+',
         help='Test arguments passed to the test executable')
     arg_parser.add_argument(
         '--build-dir', dest='build_dir', metavar='BUILD_DIR',
@@ -95,46 +102,46 @@ def parse_args(build_script):
 def run_test(test_exe, test_args):
     '''
     Run a single test
+
+    The test working directory is based on the test relative path.
+    If the test executable is not found or the test fails an error is thrown.
     '''
     run_test_args = [test_exe]
     if args.test_args is not None:
         run_test_args.extend(args.test_args)
-    run_test_work_dir = os.path.split(test_exe)[0]
+    run_test_work_dir = os.path.split(os.path.abspath(test_exe))[0]
     if '' == run_test_work_dir:
         run_test_work_dir = os.getcwd()
     try:
         test_result = subprocess.run(run_test_args, cwd=run_test_work_dir)
         if 0 != test_result.returncode:
-            raise TestError('Running test', ' '.join(run_test_args), 'failed')
+            raise TestError('Running test', ' '.join(
+                run_test_args), 'failed', ', working_dir:', run_test_work_dir)
     except Exception:
-        raise TestError('Failed to run test', test_exe, ' '.join(test_args))
+        raise TestError('Failed to run test', test_exe, ' '.join(
+            test_args), ', working_dir:', run_test_work_dir)
 
 
 # Main entry point
 if '__main__' == __name__:
-    try:
-        build_script = import_build_script()
-        args = parse_args(build_script)
-        tests = []
-        if args.test_exe is None:
-            for test in ALL_TESTS.items():
-                test_exe_dir = args.build_dir
-                if test_exe_dir is None:
-                    test_exe_dir = os.path.join(
-                        build_script.BUILD_CONFIGS[args.configuration],
-                        platform.system().lower(),
-                        args.architecture,
-                        'bin')
-                test_exe = os.path.join(test_exe_dir, test[0])
-                test_args = copy.deepcopy(test[1])
-                if args.test_args is not None:
-                    test_args.extend(args.test_args)
-                tests.append((test_exe, test_args))
-        else:
-            tests = [(args.test_exe, args.test_args)]
-        for test_exe, test_args in tests:
-            run_test(test_exe, test_args)
-    except Exception as error:
-        print(error.__class__.__name__, *(error.args))
-        sys.exit(1)
-    sys.exit(0)
+    build_script = import_build_script()
+    args = parse_args(build_script)
+    tests = []
+    if args.test_exe is None:
+        for test in ALL_TESTS.items():
+            test_exe_dir = args.build_dir
+            if test_exe_dir is None:
+                test_exe_dir = os.path.join(
+                    build_script.BUILD_CONFIGS[args.configuration],
+                    platform.system().lower(),
+                    args.architecture,
+                    'bin')
+            test_exe = os.path.join(test_exe_dir, test[0])
+            test_args = copy.deepcopy(test[1])
+            if args.test_args is not None:
+                test_args.extend(args.test_args)
+            tests.append((test_exe, test_args))
+    else:
+        tests = [(args.test_exe, args.test_args)]
+    for test_exe, test_args in tests:
+        run_test(test_exe, test_args)
