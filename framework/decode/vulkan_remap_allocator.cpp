cd009110aded57d5bd825bd33a83ba9a22858f1e
Dustin Graves <dustin@lunarg.com>
2020-02-11 19:31:37 -0700

    Add memory type index remapping
    Adds a new implementation of the VulkanResourceAllocator interface,
    which will attempt to remap memory type indices at replay when the
    replay memory types do not match the capture memory types.  Remapping
    involves comparing the captured memory types to the replay memory types
    to find a replay memory type index that is compatible with the capture
    memory type.
    
    Change-Id: I8013f3e8a828087c0a22b1df19fd97d752203f8d

---
+++ framework/decode/vulkan_remap_allocator.cpp
@@ -0,0 +1,44 @@
+/*
+** Copyright (c) 2020 LunarG, Inc.
+**
+** Licensed under the Apache License, Version 2.0 (the "License");
+** you may not use this file except in compliance with the License.
+** You may obtain a copy of the License at
+**
+**     http://www.apache.org/licenses/LICENSE-2.0
+**
+** Unless required by applicable law or agreed to in writing, software
+** distributed under the License is distributed on an "AS IS" BASIS,
+** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+** See the License for the specific language governing permissions and
+** limitations under the License.
+*/
+
+#include "decode/vulkan_remap_allocator.h"
+
+#include "decode/custom_vulkan_struct_decoders.h"
+#include "decode/vulkan_object_info.h"
+#include "generated/generated_vulkan_struct_decoders.h"
+
+GFXRECON_BEGIN_NAMESPACE(gfxrecon)
+GFXRECON_BEGIN_NAMESPACE(decode)
+
+VkResult VulkanRemapAllocator::AllocateMemory(PFN_vkAllocateMemory                                      func,
+                                              const DeviceInfo*                                         device_info,
+                                              const StructPointerDecoder<Decoded_VkMemoryAllocateInfo>& pAllocateInfo,
+                                              const VkAllocationCallbacks*                              pAllocator,
+                                              HandlePointerDecoder<VkDeviceMemory>*                     pMemory)
+{
+    const VkMemoryAllocateInfo* capture_allocate_info = pAllocateInfo.GetPointer();
+    VkMemoryAllocateInfo        replay_allocate_info  = *capture_allocate_info;
+
+    replay_allocate_info.memoryTypeIndex = index_map_[capture_allocate_info->memoryTypeIndex];
+
+    // TODO: Store the new memory type index in the DeviceMemoryInfo structure and add a check at buffer/image bind,
+    // reporting errors when binding to incompatible memory types.
+
+    return func(device_info->handle, &replay_allocate_info, pAllocator, pMemory->GetHandlePointer());
+}
+
+GFXRECON_END_NAMESPACE(decode)
+GFXRECON_END_NAMESPACE(gfxrecon)
