25fa735637eb8d2681ea3ca610d5ad156e15f759
xooi <xiangquan.ooi@amd.com>
2020-06-25 16:57:43 -0400

    replay: update --emrp argument option usage
    add console output on --emrp argument usage
    and check to make sure it is always mutually
    exclusive with -m rebind and remap mode.
    
    SWLGFXREC-28
    
    Change-Id: I0c4437778ed98a1c3e05ffbe69e24262b15ac387

--- tools/replay/replay_settings.h
+++ tools/replay/replay_settings.h
@@ -341,7 +341,8 @@ static void PrintUsage(const char* exe_name)
     GFXRECON_WRITE_CONSOLE("\t\t\t[--paused] [--sfa | --skip-failed-allocations]");
     GFXRECON_WRITE_CONSOLE("\t\t\t[--replace-shaders <dir>]");
     GFXRECON_WRITE_CONSOLE("\t\t\t[--opcd | --omit-pipeline-cache-data] [--wsi <platform>]");
-    GFXRECON_WRITE_CONSOLE("\t\t\t[-m <mode> | --memory-translation <mode>] <file>\n");
+    GFXRECON_WRITE_CONSOLE("\t\t\t[-m <mode> | --memory-translation <mode>]");
+    GFXRECON_WRITE_CONSOLE("\t\t\t[--emrp | --enable-multipass-replay-portability] <file>\n");
     GFXRECON_WRITE_CONSOLE("Required arguments:");
     GFXRECON_WRITE_CONSOLE("  <file>\t\tPath to the capture file to replay.");
     GFXRECON_WRITE_CONSOLE("\nOptional arguments:");
@@ -379,6 +380,9 @@ static void PrintUsage(const char* exe_name)
     GFXRECON_WRITE_CONSOLE("          \t\t         \tto different allocations with different");
     GFXRECON_WRITE_CONSOLE("          \t\t         \toffsets.  Uses VMA to manage allocations");
     GFXRECON_WRITE_CONSOLE("          \t\t         \tand suballocations.");
+    GFXRECON_WRITE_CONSOLE("  --emrp\t\tEnable multipass (2-pass) replay portability for replay");
+    GFXRECON_WRITE_CONSOLE("        \t\ton GPU with memory requirements that are different with");
+    GFXRECON_WRITE_CONSOLE("        \t\tcapture GPU (same as --enable-multipass-replay-portability).");
 }
 
 #endif // GFXRECON_REPLAY_SETTINGS_H
