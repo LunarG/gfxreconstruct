18e2c52a2c4d867ffc7ebeda04c815a66682cf5e
Dustin Graves <dustin@lunarg.com>
2020-07-29 15:09:21 -0600

    Fix resize2 block size
    The new kResizeWindowCommand2 meta-data blocks were written with an
    incorrect block size.  The size is only used to seek past an
    unrecognized block when processing a file, and the invalid size can
    cause a decoding error with older tools that do not support his type.
    
    Change-Id: I31387690bbd8b4eb0cb28c924c5f6de4c9baabb3

--- framework/encode/vulkan_state_writer.cpp
+++ framework/encode/vulkan_state_writer.cpp
@@ -2451,11 +2451,9 @@ void VulkanStateWriter::WriteResizeWindowCmd2(format::HandleId              surf
 {
     format::ResizeWindowCommand2 resize_cmd2;
     resize_cmd2.meta_header.block_header.type = format::BlockType::kMetaDataBlock;
-    resize_cmd2.meta_header.block_header.size = sizeof(resize_cmd2.meta_header.meta_data_type) +
-                                                sizeof(resize_cmd2.thread_id) + sizeof(resize_cmd2.surface_id) +
-                                                sizeof(resize_cmd2.width) + sizeof(resize_cmd2.height);
-    resize_cmd2.meta_header.meta_data_type = format::MetaDataType::kResizeWindowCommand2;
-    resize_cmd2.thread_id                  = thread_id_;
+    resize_cmd2.meta_header.block_header.size = sizeof(resize_cmd2) - sizeof(resize_cmd2.meta_header.block_header);
+    resize_cmd2.meta_header.meta_data_type    = format::MetaDataType::kResizeWindowCommand2;
+    resize_cmd2.thread_id                     = thread_id_;
 
     resize_cmd2.surface_id = surface_id;
     resize_cmd2.width      = width;
