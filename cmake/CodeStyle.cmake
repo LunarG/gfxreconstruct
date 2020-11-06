8f84986a1a5c032284bf5b2eae314cbb7e415850
Dustin Graves <dustin@lunarg.com>
2020-11-06 16:07:37 -0700

    Adjust code style macro logic
    Add conditional logic to the target_code_style_build_directives CMake
    function to prevent any commands from being run when code style checks
    are disabled.
    
    Change-Id: I9ba3a69b36531e626dafc7270383d55a792982d7

--- cmake/CodeStyle.cmake
+++ cmake/CodeStyle.cmake
@@ -44,27 +44,29 @@ endmacro()
 
 # Apply code style build directives
 function(target_code_style_build_directives TARGET)
-    generate_target_source_files(${TARGET} TARGET_SRC_FILES TARGET_SOURCE_FILES)
-    if(${APPLY_CPP_CODE_STYLE})
-        if(CLANG_FORMAT-NOTFOUND STREQUAL ${CLANG_FORMAT})
-            message(FATAL_ERROR "Failed to find clang-format in system path")
+    if(${APPLY_CPP_CODE_STYLE} OR ${CHECK_CPP_CODE_STYLE})
+        generate_target_source_files(${TARGET} TARGET_SRC_FILES TARGET_SOURCE_FILES)
+        if(${APPLY_CPP_CODE_STYLE})
+            if(CLANG_FORMAT-NOTFOUND STREQUAL ${CLANG_FORMAT})
+                message(FATAL_ERROR "Failed to find clang-format in system path")
+            endif()
+            # If apply code style is on, turn off check code style
+            set(CHECK_CPP_CODE_STYLE OFF)
+            add_custom_target("${TARGET}ClangFormat"
+                    COMMAND ${CLANG_FORMAT} -i ${TARGET_SRC_FILES}
+                    WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
+                    COMMENT "Run clang format for ${TARGET}"
+                    COMMAND_EXPAND_LISTS)
+            add_dependencies(${TARGET} "${TARGET}ClangFormat")
         endif()
-        # If apply code style is on, turn off check code style
-        set(CHECK_CPP_CODE_STYLE OFF)
-        add_custom_target("${TARGET}ClangFormat"
-                COMMAND ${CLANG_FORMAT} -i ${TARGET_SRC_FILES}
-                WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
-                COMMENT "Run clang format for ${TARGET}"
-                COMMAND_EXPAND_LISTS)
-        add_dependencies(${TARGET} "${TARGET}ClangFormat")
-    endif()
-    if(${CHECK_CPP_CODE_STYLE})
-        # Call the script to check formatting
-        add_custom_target("${TARGET}CodeStyleCheck"
-                COMMAND "${PYTHON}" ${CMAKE_SOURCE_DIR}/scripts/check_code_style.py
-                --sourcefile ${TARGET_SRC_FILES} --base ${CHECK_CPP_CODE_STYLE_BASE}
-                WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
-                COMMENT "Check code style for ${TARGET}")
-        add_dependencies(${TARGET} "${TARGET}CodeStyleCheck")
+        if(${CHECK_CPP_CODE_STYLE})
+            # Call the script to check formatting
+            add_custom_target("${TARGET}CodeStyleCheck"
+                    COMMAND "${PYTHON}" ${CMAKE_SOURCE_DIR}/scripts/check_code_style.py
+                    --sourcefile ${TARGET_SRC_FILES} --base ${CHECK_CPP_CODE_STYLE_BASE}
+                    WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
+                    COMMENT "Check code style for ${TARGET}")
+            add_dependencies(${TARGET} "${TARGET}CodeStyleCheck")
+        endif()
     endif()
 endfunction()
