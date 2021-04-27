49b163a5964666da0f29db9f784c60f2fbbaceff
Mike Schuchardt <mikes@lunarg.com>
2021-04-27 14:19:24 -0700

    Fix vkconfig built-in variable
    Add @ONLY to configure_file so it doesn't do substitution on the
    vkconfig ${LOCAL} variable in VkLayer_gfxreconstruct.json.in
    
    Change-Id: Id8f44c7b3a99000085da8c77db9fd8dda170ff17

--- cmake/GenerateLayerJson.cmake
+++ cmake/GenerateLayerJson.cmake
@@ -3,7 +3,7 @@ function(GENERATE_LAYER_JSON_FILE TARGET RELATIVE_PATH_PREFIX LAYER_BINARY IN_FI
     # Run each .json.in file through the generator
     # We need to create the generator.cmake script so that the generator can be run at compile time, instead of configure time
     # Running at compile time lets us use cmake generator expressions (TARGET_FILE_NAME and TARGET_FILE_DIR, specifically)
-    file(WRITE "${CMAKE_CURRENT_BINARY_DIR}/generator.cmake" "configure_file(\"\${INPUT_FILE}\" \"\${OUTPUT_FILE}\")")
+    file(WRITE "${CMAKE_CURRENT_BINARY_DIR}/generator.cmake" "configure_file(\"\${INPUT_FILE}\" \"\${OUTPUT_FILE}\" @ONLY)")
 
     # Combine major, minor, and patch version values into a single integer.
     math(EXPR GFXRECONSTRUCT_LAYER_JSON_VERSION "${GFXRECONSTRUCT_PROJECT_VERSION_MAJOR} << 22 | ${GFXRECONSTRUCT_PROJECT_VERSION_MINOR} << 12 | ${GFXRECONSTRUCT_PROJECT_VERSION_PATCH}")
