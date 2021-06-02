98fa1aec7d6a6f613853ab27a76fd2ccd838eeff
David Donaldson <daviddonaldson@lunarg.com>
2021-06-02 13:32:49 -0700

    Fix a race condition and crash when done trimming
    When a block is done trimming, the file is closed and GFXR sets a state
    variable to indicate no additional API calls should be captured to the
    file. However there was no lock around changing the trim state and the
    file close, so it was possible for a thread to try and write to file
    after it was closed. This is fixed by acquiring the unique state lock
    when opening and closing the file.
    
    Change-Id: I2655ec4430b20ee01651597410496e5b8dc1b074

--- framework/encode/trace_manager.h
+++ framework/encode/trace_manager.h
@@ -1002,6 +1002,7 @@ class TraceManager
     std::string CreateTrimFilename(const std::string& base_filename, const CaptureSettings::TrimRange& trim_range);
     bool        CreateCaptureFile(const std::string& base_filename);
     void        ActivateTrimming();
+    void        DeactivateTrimming();
 
     void WriteFileHeader();
     void BuildOptionList(const format::EnabledOptions&        enabled_options,
