ea4f396643c4c449d03c961bddbca5235b6625cb
Dror Smolarsky <dror.smolarsky@gmail.com>
2021-07-19 10:47:09 -0400

    Lint and style fixes to the capture script
    * Change functions names to lower_case
    * Change constant names to UPPER_CASE
    * Minor fixes to command line argument names stored in the command line
      argument collection
    * Fixes to indent
    * Apply lint and autopep tools to the script

--- tools/capture/gfxrecon-capture.py
+++ tools/capture/gfxrecon-capture.py
@@ -23,6 +23,8 @@
 #
 # Utility for capturing Vulkan API calls using gfxreconstruct
 
+'''Capture a trace of a Vulkan binary.
+'''
 
 import argparse
 import os
@@ -31,17 +33,20 @@ import sys
 import subprocess
 
 
-######################
-# Define a usage message.
-# We use a custom usage message because Python's arg parser
-# doesn't provide a very good usage message for positional arguments.
-# (The default usage message for positional args is
-# as an ellipse. We would rather have "<program> [<programArgs>]".)
-def UsageMsg():
+def usage_message():
+    '''Output the script usage message
+
+    We use a custom usage message because Python's arg parser doesn't provide
+    a very good usage message for positional arguments.
+    (The default usage message for positional args is as an ellipse. We would
+    rather have "<program> [<programArgs>]".)
+
+    :return: A well formatted usage message string
+    '''
     msg = ('gfxrecon-capture.py [-h]' + os.linesep +
            '                           [-w dir]' + os.linesep +
-           '                           [-o captureFile]' + os.linesep +
-           '                           [-f captureFrames]' + os.linesep +
+           '                           [-o capture_file]' + os.linesep +
+           '                           [-f capture_frames]' + os.linesep +
            '                           [--no-file-timestamp]' + os.linesep +
            '                           [--trigger {F1-F12,TAB,CTRL}]' + os.linesep +
            '                           [--compression-type {LZ4,ZLIB,ZSTD,NONE}]' + os.linesep +
@@ -55,194 +60,244 @@ def UsageMsg():
     return msg
 
 
-
-######################
-# SmartFormater is used by arg parser to assist in breaking arg help text into multiple lines.
-# It is used whenever the help text starts with 'R|'.
 class SmartFormatter(argparse.HelpFormatter):
+    '''Used by the argument parser to assist in breaking argument help text
+    into multiple lines.
+
+    It is used whenever the help text starts with 'R|'.
+    '''
+
     def _split_lines(self, text, width):
+        '''Split the argument help into multiple lines
+
+        In addition to the default behavior it will also break lines that start
+        with R|.
+        '''
         if text.startswith('R|'):
             return text[2:].splitlines()
         return argparse.HelpFormatter._split_lines(self, text, width)
 
 
-######################
-# Print error message and exit with non-zero status
-def PrintErrorAndExit(msg):
+def print_error_and_exit(msg):
+    '''Print error message and exit with non-zero status
+    '''
     print(os.path.basename(__file__) + ' error: ' + msg)
     sys.exit(1)
 
 
-######################
-# Set an environment variable to a given value or remove it from the environment if None
-def SetEnvVar(name, value):
+def set_env_var(name, value):
+    '''Set an environment variable to a given value or remove it from the
+    environment if None
+    '''
     if value is not None:
         os.environ[name] = value
     elif name in os.environ:
         del os.environ[name]
 
 
-######################
-# Parse arguments
-def ParseArgs():
+def create_argument_parser():
+    '''Create the argument parser used to parse command line arguments
 
-    triggerKeyChoices = ['F1','F2','F3','F4','F5','F6','F7','F8','F9','F10','F11','F12','TAB','CTRL']
-    compressionTypeChoices = ['LZ4','ZLIB','ZSTD','NONE']
-    logLevelChoices = ['debug','info','warn','error','fatal']
-    memoryTrackingModeChoices = ['page_guard','assisted','unassisted']
+    Create an argument parser with a custom description and useage.
+    Add command line arguments and options to the parser.
+    '''
+    TRIGGER_KEY_CHOICES = ['F1', 'F2', 'F3', 'F4', 'F5', 'F6',
+                           'F7', 'F8', 'F9', 'F10', 'F11', 'F12', 'TAB', 'CTRL']
+    COMPRESSION_CHOICES = ['LZ4', 'ZLIB', 'ZSTD', 'NONE']
+    LOG_LEVEL_CHOICES = ['debug', 'info', 'warn', 'error', 'fatal']
+    MEMORY_TRACKING_MODE_CHOICES = ['page_guard', 'assisted', 'unassisted']
 
-    parser = argparse.ArgumentParser(prog=os.path.basename(sys.argv[0]), description='Create a capture of a Vulkan program.', usage=UsageMsg(), allow_abbrev=False, formatter_class=SmartFormatter)
+    parser = argparse.ArgumentParser(
+        prog=os.path.basename(sys.argv[0]),
+        description=__doc__,
+        usage=usage_message(),
+        allow_abbrev=False,
+        formatter_class=argparse.RawTextHelpFormatter)
 
     # Common optional args
     # All arguments default to None, indicating they should be unset in the capture environment
-    parser.add_argument('-w', '--working-dir', dest='workingDir', metavar='<dir>', help='Set CWD to this directory before running the program')
-    parser.add_argument('-o', '--capture-file', dest='captureFile', metavar='<captureFile>', default='gfxrecon_capture.gfxr', help='Name of the capture file, default is gfxrecon_capture.gfxr')
-    parser.add_argument('-f', '--capture-frames', dest='captureFrames', metavar='<captureFrames>', help='List of frames to capture, default is all frames')
-    parser.add_argument('--no-file-timestamp', dest='fileTimestamp', action='store_const', const='false', help='Do not add a timestamp to the capture file name')
-    parser.add_argument('--trigger', dest='trigger', choices=triggerKeyChoices, help='Specify a hotkey to start/stop capture')
-    parser.add_argument('--compression-type', dest='compressionType', choices=compressionTypeChoices, help='Specify the type of compression to use in the capture file, default is LZ4')
-    parser.add_argument('--file-flush', dest='fileFlush', action='store_const', const='true', help='Flush output stream after each packet is written to capture file')
-    parser.add_argument('--log-level', dest='logLevel', choices=logLevelChoices, help='Specify highest level message to log, default is info')
-    parser.add_argument('--log-file', dest='logFile', metavar='<logFile>', help='Write log messages to a file at the specified path. Default is: Empty string (file logging disabled)')
-    parser.add_argument('--log-debugview', dest='logDebugView', action='store_const', const='true', help='Log messages with OutputDebugStringA' if sys.platform=='win32' else argparse.SUPPRESS)
-    parser.add_argument('--memory-tracking-mode', dest='memoryTrackingMode', choices=memoryTrackingModeChoices , help=
-                        'R|Method to use to track changes to memory mapped objects:' + os.linesep +
-                        '   page_guard: use pageguard to track changes (default)' + os.linesep +
-                        '   assisted: application will call vkFlushMappedMemoryRanges' + os.linesep +
-                        '      for memory to be written to the capture file' + os.linesep +
-                        '   unassisted: all mapped memory will be written to the' + os.linesep +
-                        '      capture file during VkQueueSubmit and VkUnmapMemory')
+    parser.add_argument(
+        '-w', '--working-dir', dest='working_dir', metavar='<dir>',
+        help='Set CWD to this directory before running the program')
+    parser.add_argument(
+        '-o', '--capture-file', dest='capture_file', metavar='<capture_file>',
+        default='gfxrecon_capture.gfxr',
+        help='Name of the capture file, default is gfxrecon_capture.gfxr')
+    parser.add_argument(
+        '-f', '--capture-frames', dest='capture_frames',
+        metavar='<capture_frames>',
+        help='List of frames to capture, default is all frames')
+    parser.add_argument(
+        '--no-file-timestamp', dest='file_timestamp', action='store_const', const='false',
+        help='Do not add a timestamp to the capture file name')
+    parser.add_argument(
+        '--trigger', dest='trigger', choices=TRIGGER_KEY_CHOICES,
+        help='Specify a hotkey to start/stop capture')
+    parser.add_argument(
+        '--compression-type', dest='compression_type', choices=COMPRESSION_CHOICES,
+        help='Specify the type of compression to use in the capture file, default is LZ4')
+    parser.add_argument(
+        '--file-flush', dest='file_flush', action='store_const', const='true',
+        help='Flush output stream after each packet is written to capture file')
+    parser.add_argument(
+        '--log-level', dest='log_level', choices=LOG_LEVEL_CHOICES,
+        help='Specify highest level message to log, default is info')
+    parser.add_argument(
+        '--log-file', dest='log_file', metavar='<log_file>',
+        help='Write log messages to a file at the specified path. Default is: Empty string (file logging disabled)')
+    parser.add_argument(
+        '--log-debugview', dest='log_debug_view', action='store_const', const='true',
+        help='Log messages with OutputDebugStringA' if sys.platform == 'win32' else argparse.SUPPRESS)
+    parser.add_argument(
+        '--memory-tracking-mode', dest='memory_tracking_mode', choices=MEMORY_TRACKING_MODE_CHOICES,
+        help='R|Method to use to track changes to memory mapped objects:' + os.linesep +
+             '   page_guard: use pageguard to track changes (default)' + os.linesep +
+             '   assisted: application will call vkFlushMappedMemoryRanges' + os.linesep +
+             '      for memory to be written to the capture file' + os.linesep +
+             '   unassisted: all mapped memory will be written to the' + os.linesep +
+             '      capture file during VkQueueSubmit and VkUnmapMemory')
 
     # Required args
-    parser.add_argument('programAndArgs', metavar='<program> [<program args>]', nargs=argparse.REMAINDER, help='Program to capture, optionally followed by program arguments')
+    parser.add_argument(
+        'program_and_args', metavar='<program> [<program args>]', nargs=argparse.REMAINDER,
+        help='Program to capture, optionally followed by program arguments')
 
     return parser
 
 
-######################
-# PrintArgs - for debugging
-def PrintArgs(args):
-    print('working-dir', args.workingDir)
-    print('capture-file', os.path.abspath(args.captureFile))
-    print('capture-frames', args.captureFrames)
-    print('no-file-timestamp', args.fileTimestamp)
+def print_args(args):
+    '''Print command line argument values.
+
+    Used for debug.
+    '''
+    print('working-dir', args.working_dir)
+    print('capture-file', os.path.abspath(args.capture_file))
+    print('capture-frames', args.capture_frames)
+    print('no-file-timestamp', args.file_timestamp)
     print('trigger', args.trigger)
-    print('compression-type', args.compressionType)
-    print('file-flush', args.fileFlush)
-    print('log-level', args.logLevel)
-    print('log-file', args.logFile)
-    print('log-debugview', args.logDebugView)
-    print('memory-tracking-mode', args.memoryTrackingMode)
-    print('programAndArgs', args.programAndArgs)
+    print('compression-type', args.compression_type)
+    print('file-flush', args.file_flush)
+    print('log-level', args.log_level)
+    print('log-file', args.log_file)
+    print('log-debugview', args.log_debug_view)
+    print('memory-tracking-mode', args.memory_tracking_mode)
+    print('program_and_args', args.program_and_args)
 
 
-######################
-# Get the full path to the command to execute
 def GetCommandPath(args):
+    '''Get the full path to the command to execute
+    '''
     # Replace ~ or ~user with the user's home path before calling shutil.which()
-    programName=os.path.expanduser(args.programAndArgs[0])
+    programName = os.path.expanduser(args.program_and_args[0])
     return shutil.which(programName)
 
-######################
-# Do validation on arguments
+
 def ValidateArgs(args):
+    '''Validate command line arguments
+    '''
+    # Verify working_dir exists and is a directory.
+    if args.working_dir is not None:
+        if (not os.path.isdir(args.working_dir)):
+            print_error_and_exit('Working directory ' +
+                                 args.working_dir + ' does not exist')
 
-    # Verify workingDir exists and is a directory.
-    if args.workingDir is not None:
-        if (not os.path.isdir(args.workingDir)):
-            PrintErrorAndExit('Working directory ' + args.workingDir + ' does not exist')
-
-    # Make sure program was specified. arg parser doesn't allow specifying programAndArgs
+    # Make sure program was specified. arg parser doesn't allow specifying program_and_args
     # as required.
-    if len(args.programAndArgs) == 0:
-        print('usage: ' + UsageMsg())
-        PrintErrorAndExit('<program> must be specified')
+    if len(args.program_and_args) == 0:
+        print('usage: ' + usage_message())
+        print_error_and_exit('<program> must be specified')
 
     # Verify programName exists and is executable.
     if GetCommandPath(args) is None:
-        PrintErrorAndExit('Cannot find program ' + programName + ' to execute')
+        print_error_and_exit('Cannot find program ' +
+                             programName + ' to execute')
 
-    # Verify captureFile directory exists and is a valid directory.
-    captureFileDir = os.path.dirname(os.path.abspath(args.captureFile))
-    if (not os.path.exists(captureFileDir)):
-        PrintErrorAndExit('Capture file output directory ' + captureFileDir + ' does not exist')
-    if (not os.path.isdir(captureFileDir)):
-        PrintErrorAndExit('Capture file output directory ' + captureFileDir + ' is not a valid directory')
+    # Verify capture_file directory exists and is a valid directory.
+    capture_fileDir = os.path.dirname(os.path.abspath(args.capture_file))
+    if (not os.path.exists(capture_fileDir)):
+        print_error_and_exit('Capture file output directory ' +
+                             capture_fileDir + ' does not exist')
+    if (not os.path.isdir(capture_fileDir)):
+        print_error_and_exit('Capture file output directory ' +
+                             capture_fileDir + ' is not a valid directory')
 
 
-######################
-# Set env variables for capture layer
-def SetEnvVars(args):
-
+def set_env_vars(args):
+    '''Set environment variables for capture layer
+    '''
     # Set VK_INSTANCE_LAYERS
     # If gfxr layer is not already in VK_INSTANCE_LAYER, append gfxr layer to VK_INSTANCE_LAYERS
     if os.getenv('VK_INSTANCE_LAYERS') is None:
         os.environ['VK_INSTANCE_LAYERS'] = 'VK_LAYER_LUNARG_gfxreconstruct'
     elif (not ('VK_LAYER_LUNARG_gfxreconstruct' in os.getenv('VK_INSTANCE_LAYERS'))):
-        os.environ['VK_INSTANCE_LAYERS'] = os.environ['VK_INSTANCE_LAYERS'] + os.pathsep + 'VK_LAYER_LUNARG_gfxreconstruct'
+        os.environ['VK_INSTANCE_LAYERS'] = os.environ['VK_INSTANCE_LAYERS'] + \
+            os.pathsep + 'VK_LAYER_LUNARG_gfxreconstruct'
 
     # Set GFXRECON_* capture options
     # The capture layer will validate these options and generate errors as needed
-    SetEnvVar('GFXRECON_CAPTURE_FILE', os.path.abspath(args.captureFile))
-    SetEnvVar('GFXRECON_CAPTURE_FRAMES', args.captureFrames)
-    SetEnvVar('GFXRECON_CAPTURE_FILE_TIMESTAMP', args.fileTimestamp)
-    SetEnvVar('GFXRECON_CAPTURE_TRIGGER', args.trigger)
-    SetEnvVar('GFXRECON_CAPTURE_COMPRESSION_TYPE', args.compressionType)
-    SetEnvVar('GFXRECON_CAPTURE_FILE_FLUSH', args.fileFlush)
-    SetEnvVar('GFXRECON_LOG_LEVEL', args.logLevel)
-    SetEnvVar('GFXRECON_LOG_FILE', args.logFile)
-    SetEnvVar('GFXRECON_LOG_OUTPUT_TO_OS_DEBUG_STRING', args.logDebugView)
-    SetEnvVar('GFXRECON_MEMORY_TRACKING_MODE', args.memoryTrackingMode)
+    set_env_var('GFXRECON_CAPTURE_FILE', os.path.abspath(args.capture_file))
+    set_env_var('GFXRECON_CAPTURE_FRAMES', args.capture_frames)
+    set_env_var('GFXRECON_CAPTURE_FILE_TIMESTAMP', args.file_timestamp)
+    set_env_var('GFXRECON_CAPTURE_TRIGGER', args.trigger)
+    set_env_var('GFXRECON_CAPTURE_COMPRESSION_TYPE', args.compression_type)
+    set_env_var('GFXRECON_CAPTURE_FILE_FLUSH', args.file_flush)
+    set_env_var('GFXRECON_LOG_LEVEL', args.log_level)
+    set_env_var('GFXRECON_LOG_FILE', args.log_file)
+    set_env_var('GFXRECON_LOG_OUTPUT_TO_OS_DEBUG_STRING', args.log_debug_view)
+    set_env_var('GFXRECON_MEMORY_TRACKING_MODE', args.memory_tracking_mode)
 
 
-######################
-# PrintEnvVar - for debugging
-# print a single env var
-def PrintEnvVar(envVar):
-    if envVar in os.environ:
-        print(envVar, os.environ[envVar])
+def print_env_var(env_var):
+    '''Print the given envrionment variable
+
+    Used for debug.
+    :param env_var: The envrionment variable printed
+    '''
+    if env_var in os.environ:
+        print(env_var, os.environ[env_var])
     else:
-        print(envVar, 'None')
+        print(env_var, 'None')
 
 
-######################
-# PrintLayerEnv - for debugging
-# print all env vars used by layer that are set by this script
 def PrintLayerEnv():
-    PrintEnvVar('GFXRECON_CAPTURE_COMPRESSION_TYPE')
-    PrintEnvVar('GFXRECON_CAPTURE_FILE')
-    PrintEnvVar('GFXRECON_CAPTURE_FILE_FLUSH')
-    PrintEnvVar('GFXRECON_CAPTURE_FILE_TIMESTAMP')
-    PrintEnvVar('GFXRECON_CAPTURE_FRAMES')
-    PrintEnvVar('GFXRECON_CAPTURE_TRIGGER')
-    PrintEnvVar('GFXRECON_LOG_FILE')
-    PrintEnvVar('GFXRECON_LOG_LEVEL')
-    PrintEnvVar('GFXRECON_LOG_OUTPUT_TO_OS_DEBUG_STRING')
-    PrintEnvVar('GFXRECON_MEMORY_TRACKING_MODE')
-    PrintEnvVar('VK_INSTANCE_LAYERS')
+    '''Print all GFXReconstruct layer environment variables
+
+    Used for debug.
+    '''
+    print_env_var('GFXRECON_CAPTURE_COMPRESSION_TYPE')
+    print_env_var('GFXRECON_CAPTURE_FILE')
+    print_env_var('GFXRECON_CAPTURE_FILE_FLUSH')
+    print_env_var('GFXRECON_CAPTURE_FILE_TIMESTAMP')
+    print_env_var('GFXRECON_CAPTURE_FRAMES')
+    print_env_var('GFXRECON_CAPTURE_TRIGGER')
+    print_env_var('GFXRECON_LOG_FILE')
+    print_env_var('GFXRECON_LOG_LEVEL')
+    print_env_var('GFXRECON_LOG_OUTPUT_TO_OS_DEBUG_STRING')
+    print_env_var('GFXRECON_MEMORY_TRACKING_MODE')
+    print_env_var('VK_INSTANCE_LAYERS')
 
 
-if __name__ == '__main__':
+if '__main__' == __name__:
 
     # We don't support running under Cygwin Python
     if sys.platform == 'cygwin':
-        PrintErrorAndExit("Cygwin Python not supported")
+        print_error_and_exit("Cygwin Python not supported")
 
     # Get and validate args
-    parser = ParseArgs()
+    parser = create_argument_parser()
     args = parser.parse_args()
-    #PrintArgs(args)   # For debugging
+    # print_args(args)   # For debugging
     ValidateArgs(args)
 
     # Set up environment
-    SetEnvVars(args)
-    #PrintLayerEnv()    # For debugging
+    set_env_vars(args)
+    # PrintLayerEnv()    # For debugging
 
-    # If workingDir was specified, make it the cwd
-    if args.workingDir is not None:
-        os.chdir(args.workingDir)
+    # If working_dir was specified, make it the cwd
+    if args.working_dir is not None:
+        os.chdir(args.working_dir)
 
     # Run the program and and exit with the exit status of the program
     print('Executing program', GetCommandPath(args))
-    result = subprocess.run(args.programAndArgs)
+    result = subprocess.run(args.program_and_args)
     sys.exit(result.returncode)
