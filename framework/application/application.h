5b7e1409acae5b35209021453e0504167e5ea9a1
patrick-lunarg <patrick@lunarg.com>
2021-11-10 16:10:09 -0800

    General cleanup

--- framework/application/application.h
+++ framework/application/application.h
@@ -73,13 +73,13 @@ class Application final
 
   private:
     // clang-format off
-    std::string                 name_;           ///< Application name to display in window title bar.
-    decode::FileProcessor*      file_processor_; ///< The FileProcessor object responsible for decoding and processing capture file data.
-    bool                        running_;        ///< Indicates that the application is actively processing system events for playback.
-    bool                        paused_;         ///< Indicates that the playback has been paused.  When paused the application will stop rendering, but will continue processing system events.
-    uint32_t                    pause_frame_;    ///< The number for a frame that replay should pause after.
-    std::unordered_map<std::string, std::unique_ptr<WsiContext>> wsi_contexts_; ///< TODO : Documentation
-    std::string cli_wsi_extension_; ///< TODO : Documentation
+    std::string                                                  name_;              ///< Application name to display in window title bar.
+    decode::FileProcessor*                                       file_processor_;    ///< The FileProcessor object responsible for decoding and processing capture file data.
+    bool                                                         running_;           ///< Indicates that the application is actively processing system events for playback.
+    bool                                                         paused_;            ///< Indicates that the playback has been paused.  When paused the application will stop rendering, but will continue processing system events.
+    uint32_t                                                     pause_frame_;       ///< The number for a frame that replay should pause after.
+    std::unordered_map<std::string, std::unique_ptr<WsiContext>> wsi_contexts_;      ///< Loaded WSI contexts from CLI and VkInstanceCreateInfo
+    std::string                                                  cli_wsi_extension_; ///< WSI extension selected on CLI, empty string if no CLI selection
     // clang-format on
 };
 
