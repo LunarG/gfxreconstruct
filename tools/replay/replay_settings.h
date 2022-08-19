d2d5c962a5e1d4e38058194659881ea69193cbb3
Rodrigo Urra <Rodrigo.Urra@amd.com>
2022-08-19 15:17:51 -0400

    Separate common/Vulkan/D3D12 options usage output

--- tools/replay/replay_settings.h
+++ tools/replay/replay_settings.h
@@ -79,11 +79,6 @@ static void PrintUsage(const char* exe_name)
 #if defined(WIN32)
     GFXRECON_WRITE_CONSOLE("  --log-debugview\tLog messages with OutputDebugStringA.");
 #endif
-    GFXRECON_WRITE_CONSOLE("  --gpu <index>\t\tUse the specified device for replay, where index");
-    GFXRECON_WRITE_CONSOLE("          \t\tis the zero-based index to the array of physical devices");
-    GFXRECON_WRITE_CONSOLE("          \t\treturned by vkEnumeratePhysicalDevices.  Replay may fail");
-    GFXRECON_WRITE_CONSOLE("          \t\tif the specified device is not compatible with the");
-    GFXRECON_WRITE_CONSOLE("          \t\toriginal capture devices.");
     GFXRECON_WRITE_CONSOLE("  --pause-frame <N>\tPause after replaying frame number N.");
     GFXRECON_WRITE_CONSOLE("  --paused\t\tPause after replaying the first frame (same");
     GFXRECON_WRITE_CONSOLE("          \t\tas --pause-frame 1).");
@@ -112,6 +107,21 @@ static void PrintUsage(const char* exe_name)
     GFXRECON_WRITE_CONSOLE("          \t\tPrefix to apply to the screenshot file name.  Default is ");
     GFXRECON_WRITE_CONSOLE("          \t\t\"screenshot\", producing file names similar to");
     GFXRECON_WRITE_CONSOLE("          \t\t\"screenshot_frame_8049.bmp\".");
+    GFXRECON_WRITE_CONSOLE("  --validate\t\tEnables the Khronos Vulkan validation layer when replaying a");
+    GFXRECON_WRITE_CONSOLE("            \t\tVulkan capture or the Direct3D debug layer when replaying a");
+    GFXRECON_WRITE_CONSOLE("            \t\tDirect3D 12 capture.");
+#if defined(WIN32)
+    GFXRECON_WRITE_CONSOLE("")
+    GFXRECON_WRITE_CONSOLE("Windows-only:")
+    GFXRECON_WRITE_CONSOLE("  --api <api>\t\tUse the specified API for replay");
+    GFXRECON_WRITE_CONSOLE("          \t\tAvailable values are:");
+    GFXRECON_WRITE_CONSOLE("          \t\t    %s\tReplay with the Vulkan API enabled.", kApiFamilyVulkan);
+    GFXRECON_WRITE_CONSOLE("          \t\t    %s\tReplay with the Direct3D API enabled.", kApiFamilyD3D12);
+    GFXRECON_WRITE_CONSOLE("          \t\t    %s\t\tReplay with both the Vulkan and Direct3D 12 APIs", kApiFamilyAll);
+    GFXRECON_WRITE_CONSOLE("          \t\t         \tenabled. This is the default.");
+#endif
+    GFXRECON_WRITE_CONSOLE("")
+    GFXRECON_WRITE_CONSOLE("Vulkan-only:")
     GFXRECON_WRITE_CONSOLE("  --sfa\t\t\tSkip vkAllocateMemory, vkAllocateCommandBuffers, and");
     GFXRECON_WRITE_CONSOLE("       \t\t\tvkAllocateDescriptorSets calls that failed during");
     GFXRECON_WRITE_CONSOLE("       \t\t\tcapture (same as --skip-failed-allocations).");
@@ -131,24 +141,11 @@ static void PrintUsage(const char* exe_name)
     GFXRECON_WRITE_CONSOLE("  --sync\t\tSynchronize after each queue submission with vkQueueWaitIdle.");
     GFXRECON_WRITE_CONSOLE("  --remove-unsupported\tRemove unsupported extensions and features from instance");
     GFXRECON_WRITE_CONSOLE("                      \tand device creation parameters.");
-#if defined(WIN32)
-    GFXRECON_WRITE_CONSOLE("  --dcp\t\tDiscard (force to null) CachedPSO data to Direct3D calls that");
-    GFXRECON_WRITE_CONSOLE("       \t\tcreate a graphics or compute pipeline state or load a graphics");
-    GFXRECON_WRITE_CONSOLE("       \t\tor compute pipeline.  (May allow replay of some captures on a");
-    GFXRECON_WRITE_CONSOLE("       \t\tdifferent GPU. Same as --discard-cached-psos).");
-    GFXRECON_WRITE_CONSOLE("  --validate\t\tEnables the Khronos Vulkan validation layer when replaying a");
-    GFXRECON_WRITE_CONSOLE("            \t\tVulkan capture or the Direct3D debug layer when replaying a");
-    GFXRECON_WRITE_CONSOLE("            \t\tDirect3D 12 capture.");
-    GFXRECON_WRITE_CONSOLE("  --debug-device-lost\t\tEnables automatic injection of breadcrumbs into command buffers");
-    GFXRECON_WRITE_CONSOLE("            \t\tand page fault reporting.");
-    GFXRECON_WRITE_CONSOLE("            \t\tUsed to debug Direct3D 12 device removed problems.");
-    GFXRECON_WRITE_CONSOLE("            \t\tNot currently supported for Vulkan.");
-    GFXRECON_WRITE_CONSOLE("  --create-dummy-allocations\t\tEnables creation of dummy heaps and resources");
-    GFXRECON_WRITE_CONSOLE("            \t\tfor replay validation.");
-    GFXRECON_WRITE_CONSOLE("            \t\tNot currently supported for Vulkan.");
-#else
-    GFXRECON_WRITE_CONSOLE("  --validate\t\tEnables the Khronos Vulkan validation layer.");
-#endif
+    GFXRECON_WRITE_CONSOLE("  --gpu <index>\t\tUse the specified device for replay, where index");
+    GFXRECON_WRITE_CONSOLE("          \t\tis the zero-based index to the array of physical devices");
+    GFXRECON_WRITE_CONSOLE("          \t\treturned by vkEnumeratePhysicalDevices.  Replay may fail");
+    GFXRECON_WRITE_CONSOLE("          \t\tif the specified device is not compatible with the");
+    GFXRECON_WRITE_CONSOLE("          \t\toriginal capture devices.");
     GFXRECON_WRITE_CONSOLE("  -m <mode>\t\tEnable memory translation for replay on GPUs with memory");
     GFXRECON_WRITE_CONSOLE("          \t\ttypes that are not compatible with the capture GPU's");
     GFXRECON_WRITE_CONSOLE("          \t\tmemory types.  Available modes are:");
@@ -166,18 +163,25 @@ static void PrintUsage(const char* exe_name)
     GFXRECON_WRITE_CONSOLE("          \t\t         \tto different allocations with different");
     GFXRECON_WRITE_CONSOLE("          \t\t         \toffsets.  Uses VMA to manage allocations");
     GFXRECON_WRITE_CONSOLE("          \t\t         \tand suballocations.");
-    GFXRECON_WRITE_CONSOLE("  --fw <width,height>\tSetup windowed and override resolution(same as --force-windowed).");
 #if defined(WIN32)
-    GFXRECON_WRITE_CONSOLE("  --api <api>\t\tUse the specified API for replay (Windows only).");
-    GFXRECON_WRITE_CONSOLE("          \t\tAvailable values are:");
-    GFXRECON_WRITE_CONSOLE("          \t\t    %s\tReplay with the Vulkan API enabled.", kApiFamilyVulkan);
-    GFXRECON_WRITE_CONSOLE("          \t\t    %s\tReplay with the Direct3D API enabled.", kApiFamilyD3D12);
-    GFXRECON_WRITE_CONSOLE("          \t\t    %s\t\tReplay with both the Vulkan and Direct3D 12 APIs", kApiFamilyAll);
-    GFXRECON_WRITE_CONSOLE("          \t\t         \tenabled. This is the default.");
+    GFXRECON_WRITE_CONSOLE("")
+    GFXRECON_WRITE_CONSOLE("D3D12-only:")
+    GFXRECON_WRITE_CONSOLE("  --dcp\t\t\tForce CachedPSO to null when creating graphics or compute PSOs.");
+    GFXRECON_WRITE_CONSOLE("       \t\t\tCan help enable replay across changing driver installs.");
+    GFXRECON_WRITE_CONSOLE("       \t\t\t(Same as --discard-cached-psos)");
+    GFXRECON_WRITE_CONSOLE("  --debug-device-lost\tEnables automatic injection of breadcrumbs into command buffers");
+    GFXRECON_WRITE_CONSOLE("            \t\tand page fault reporting.");
+    GFXRECON_WRITE_CONSOLE("            \t\tUsed to debug Direct3D 12 device removed problems.");
+    GFXRECON_WRITE_CONSOLE("  --fw <width,height>\tSetup windowed and override resolution.");
+    GFXRECON_WRITE_CONSOLE("                     \t(Same as --force-windowed)");
+    GFXRECON_WRITE_CONSOLE("  --create-dummy-allocations Enables creation of dummy heaps and resources");
+    GFXRECON_WRITE_CONSOLE("                             for replay validation.");
+
+#endif
+
 #if defined(_DEBUG)
     GFXRECON_WRITE_CONSOLE("  --no-debug-popup\tDisable the 'Abort, Retry, Ignore' message box");
     GFXRECON_WRITE_CONSOLE("       \t\t\tdisplayed when abort() is called (Windows debug only).");
-#endif
 #endif
 }
 
