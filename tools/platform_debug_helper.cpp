f44aa725bbe04aefaf9b8f753fcabe66fc2b4654
Brad Grantham <brad@lunarg.com>
2023-06-28 16:59:24 -0700

    set assert, abort, and _CrtDebugReport to not open a dialog

--- tools/platform_debug_helper.cpp
+++ tools/platform_debug_helper.cpp
@@ -31,6 +31,7 @@
 #endif
 
 #include <windows.h>
+#include <crtdbg.h>
 
 DWORD DisableDebugPopup(void)
 {
@@ -61,6 +62,15 @@ DWORD DisableDebugPopup(void)
         if (not_all_spaces && !valid_zero)
         {
             _set_abort_behavior(0, _WRITE_ABORT_MSG | _CALL_REPORTFAULT);
+            _set_error_mode(_OUT_TO_STDERR);
+            SetErrorMode(SEM_FAILCRITICALERRORS | SEM_NOGPFAULTERRORBOX | SEM_NOOPENFILEERRORBOX);
+            _CrtSetReportMode(_CRT_ASSERT, _CRTDBG_MODE_FILE);
+            _CrtSetReportFile(_CRT_ASSERT, _CRTDBG_FILE_STDERR);
+            _CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_FILE);
+            _CrtSetReportFile(_CRT_WARN, _CRTDBG_FILE_STDERR);
+            _CrtSetReportMode(_CRT_ERROR, _CRTDBG_MODE_FILE);
+            _CrtSetReportFile(_CRT_ERROR, _CRTDBG_FILE_STDERR);
+
         }
     }
 
