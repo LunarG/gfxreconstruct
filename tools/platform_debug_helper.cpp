e884f4c7785260bb65245eeac2278aebc9a35c3c
Brad Grantham <brad@lunarg.com>
2023-06-28 17:02:45 -0700

    fix clang-format

--- tools/platform_debug_helper.cpp
+++ tools/platform_debug_helper.cpp
@@ -70,7 +70,6 @@ DWORD DisableDebugPopup(void)
             _CrtSetReportFile(_CRT_WARN, _CRTDBG_FILE_STDERR);
             _CrtSetReportMode(_CRT_ERROR, _CRTDBG_MODE_FILE);
             _CrtSetReportFile(_CRT_ERROR, _CRTDBG_FILE_STDERR);
-
         }
     }
 
