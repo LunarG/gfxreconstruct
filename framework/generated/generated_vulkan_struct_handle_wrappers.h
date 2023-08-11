8c967a756dcb019d8d722f93373f518d149c1569
Ming Zheng <Ming.Zheng@amd.com>
2023-08-11 10:05:25 -0400

    Update copyright info
    Update copyright info for all changed files. Please note that the change
    in gencode.py is only for adding additional copyright info to auto
    generated files.
    
    Change-Id: I211e4433da32682804d6138aa8c0820feb1ce7e6

--- framework/generated/generated_vulkan_struct_handle_wrappers.h
+++ framework/generated/generated_vulkan_struct_handle_wrappers.h
@@ -1,6 +1,7 @@
 /*
 ** Copyright (c) 2018-2023 Valve Corporation
 ** Copyright (c) 2018-2023 LunarG, Inc.
+** Copyright (c) 2023 Advanced Micro Devices, Inc.
 **
 ** Permission is hereby granted, free of charge, to any person obtaining a
 ** copy of this software and associated documentation files (the "Software"),
