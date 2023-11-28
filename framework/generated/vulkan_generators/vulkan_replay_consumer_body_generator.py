13194dd96ef674b18b5a1183fc162c759791d0a2
Ming Zheng <Ming.Zheng@amd.com>
2023-11-28 10:47:59 -0500

    Add AMD copyright info

--- framework/generated/vulkan_generators/vulkan_replay_consumer_body_generator.py
+++ framework/generated/vulkan_generators/vulkan_replay_consumer_body_generator.py
@@ -2,6 +2,7 @@
 #
 # Copyright (c) 2018-2020 Valve Corporation
 # Copyright (c) 2018-2023 LunarG, Inc.
+# Copyright (c) 2023 Advanced Micro Devices, Inc. All rights reserved.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to
