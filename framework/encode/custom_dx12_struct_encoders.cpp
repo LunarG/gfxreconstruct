1d577c94373144b27c91062bd0ee65514245bddd
Dustin Graves <quic_dgraves@quicinc.com>
2023-12-06 14:54:39 -0700

    Fix DX12 render pass union encode/decode
    Fix encoding/decoding for the D3D12_RENDER_PASS_BEGINNING_ACCESS and
    D3D12_RENDER_PASS_ENDING_ACCESS union members:
    - The Clear/Resolve union members were always encoded/decoded, regardless
      of render pass access type, resulting in uninitialized pointers being dereferenced
      when the type was not clear or resolve.
    - A new union member, PreserveLocal, added by the Agility SDK, was not being
      processed for encoding/decoding.

--- framework/encode/custom_dx12_struct_encoders.cpp
+++ framework/encode/custom_dx12_struct_encoders.cpp
@@ -399,13 +399,51 @@ void EncodeStruct(ParameterEncoder* encoder, const D3D12_VERSIONED_DEVICE_REMOVE
 void EncodeStruct(ParameterEncoder* encoder, const D3D12_RENDER_PASS_BEGINNING_ACCESS& value)
 {
     encoder->EncodeEnumValue(value.Type);
-    EncodeStruct(encoder, value.Clear);
+
+    switch (value.Type)
+    {
+        case D3D12_RENDER_PASS_BEGINNING_ACCESS_TYPE_CLEAR:
+            EncodeStruct(encoder, value.Clear);
+            break;
+        case D3D12_RENDER_PASS_BEGINNING_ACCESS_TYPE_PRESERVE_LOCAL_RENDER:
+        case D3D12_RENDER_PASS_BEGINNING_ACCESS_TYPE_PRESERVE_LOCAL_SRV:
+        case D3D12_RENDER_PASS_BEGINNING_ACCESS_TYPE_PRESERVE_LOCAL_UAV:
+            EncodeStruct(encoder, value.PreserveLocal);
+            break;
+        case D3D12_RENDER_PASS_BEGINNING_ACCESS_TYPE_DISCARD:
+        case D3D12_RENDER_PASS_BEGINNING_ACCESS_TYPE_PRESERVE:
+        case D3D12_RENDER_PASS_BEGINNING_ACCESS_TYPE_NO_ACCESS:
+            // These cases have no additional values to encode.
+            break;
+        default:
+            GFXRECON_LOG_FATAL_ONCE("Unrecognized D3D12_RENDER_PASS_BEGINNING_ACCESS union type %u", value.Type);
+            break;
+    }
 }
 
 void EncodeStruct(ParameterEncoder* encoder, const D3D12_RENDER_PASS_ENDING_ACCESS& value)
 {
     encoder->EncodeEnumValue(value.Type);
-    EncodeStruct(encoder, value.Resolve);
+
+    switch (value.Type)
+    {
+        case D3D12_RENDER_PASS_ENDING_ACCESS_TYPE_RESOLVE:
+            EncodeStruct(encoder, value.Resolve);
+            break;
+        case D3D12_RENDER_PASS_ENDING_ACCESS_TYPE_PRESERVE_LOCAL_RENDER:
+        case D3D12_RENDER_PASS_ENDING_ACCESS_TYPE_PRESERVE_LOCAL_SRV:
+        case D3D12_RENDER_PASS_ENDING_ACCESS_TYPE_PRESERVE_LOCAL_UAV:
+            EncodeStruct(encoder, value.PreserveLocal);
+            break;
+        case D3D12_RENDER_PASS_ENDING_ACCESS_TYPE_DISCARD:
+        case D3D12_RENDER_PASS_ENDING_ACCESS_TYPE_PRESERVE:
+        case D3D12_RENDER_PASS_ENDING_ACCESS_TYPE_NO_ACCESS:
+            // These cases have no additional values to encode.
+            break;
+        default:
+            GFXRECON_LOG_FATAL_ONCE("Unrecognized D3D12_RENDER_PASS_ENDING_ACCESS union type %u", value.Type);
+            break;
+    }
 }
 
 void EncodeStruct(ParameterEncoder* encoder, const LARGE_INTEGER& value)
