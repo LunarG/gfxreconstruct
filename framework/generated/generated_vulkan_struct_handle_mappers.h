e2df160d28d39702d572a8989fa727a70302fac7
Mark Young <marky@lunarg.com>
2024-04-09 09:45:22 -0600

    vulkan: Rename vulkan files with prefix

--- framework/generated/generated_vulkan_struct_handle_mappers.h
+++ framework/generated/generated_vulkan_struct_handle_mappers.h
@@ -30,8 +30,8 @@
 #ifndef  GFXRECON_GENERATED_VULKAN_STRUCT_HANDLE_MAPPERS_H
 #define  GFXRECON_GENERATED_VULKAN_STRUCT_HANDLE_MAPPERS_H
 
-#include "decode/pnext_node.h"
 #include "decode/vulkan_object_info_table.h"
+#include "decode/vulkan_pnext_node.h"
 #include "format/platform_types.h"
 #include "decode/custom_vulkan_struct_handle_mappers.h"
 #include "generated/generated_vulkan_struct_decoders_forward.h"
