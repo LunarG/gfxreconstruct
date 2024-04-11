2cce68c9d04a6a97b7b16e6c1e31fcfdd4b623bd
shabei <shabei.chen@amd.com>
2024-04-11 17:35:05 -0400

    Update according to the comments

--- framework/encode/capture_manager.cpp
+++ framework/encode/capture_manager.cpp
@@ -46,8 +46,6 @@ GFXRECON_BEGIN_NAMESPACE(encode)
 // One based frame count.
 const uint32_t kFirstFrame           = 1;
 const size_t   kFileStreamBufferSize = 256 * 1024;
-const uint32_t kPresentTestFrame     = 0x1;
-
 
 std::mutex                                     CaptureManager::ThreadData::count_lock_;
 format::ThreadId                               CaptureManager::ThreadData::thread_count_ = 0;
@@ -755,9 +753,7 @@ void CaptureManager::WriteFrameMarker(format::MarkerType marker_type)
     }
 }
 
-void CaptureManager::EndFrame(uint32_t flags)
-{
-    if (!(flags & kPresentTestFrame))
+void CaptureManager::EndFrame()
 {
     // Write an end-of-frame marker to the capture file.
     WriteFrameMarker(format::MarkerType::kEndMarker);
@@ -778,7 +774,6 @@ void CaptureManager::EndFrame(uint32_t flags)
             // Check for start of capture frame range or hotkey trigger to start capture
             CheckStartCaptureForTrackMode(current_frame_);
         }
-    }
     }
 
     // Flush after presents to help avoid capture files with incomplete final blocks.
