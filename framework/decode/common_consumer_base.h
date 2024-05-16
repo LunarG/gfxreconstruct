0ae200bf58bd8b53054b9b8c0e90cfc6d93c5e74
beau-lunarg <beau@lunarg.com>
2024-05-16 19:23:38 -0400

    Remove duplicate Process_ExeFileInfo

--- framework/decode/common_consumer_base.h
+++ framework/decode/common_consumer_base.h
@@ -50,8 +50,6 @@ class CommonConsumerBase : public MetadataConsumerBase, public MarkerConsumerBas
 
     virtual bool IsComplete(uint64_t block_index) { return false; }
 
-    virtual void Process_ExeFileInfo(util::filepath::FileInfo& info_record) {}
-
     virtual void SetCurrentBlockIndex(uint64_t block_index) override { block_index_ = block_index; }
 };
 
