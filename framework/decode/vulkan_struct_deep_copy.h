f11d27c6906fa8d649fb81f6c960af96ad3070f3
Fabian Schmidt <fabian@lunarg.com>
2024-06-25 10:07:25 +0200

    working toward a generic generator-pattern, deduce types instead of hardcoding

--- framework/decode/vulkan_struct_deep_copy.h
+++ framework/decode/vulkan_struct_deep_copy.h
@@ -54,10 +54,9 @@ inline uint8_t* offset_ptr(uint8_t* ptr, uint32_t offset)
 }
 
 //! create a helper-lambda to handle pointer-type struct-members
-auto create_handle_pointer_member_function = [](uint8_t* out_data, uint32_t i, uint64_t &offset, auto &base_struct)
-{
-    auto handle_pointer_member = [out_data, i, &offset, &base_struct](const auto &pointer_member, uint32_t count) {
-        using struct_type = std::decay_t<decltype(base_struct)>;
+auto create_handle_pointer_member_function = [](uint8_t* out_data, uint32_t i, uint64_t& offset, auto& base_struct) {
+    auto handle_pointer_member = [out_data, i, &offset, &base_struct](const auto& pointer_member, uint32_t count) {
+        using struct_type              = std::decay_t<decltype(base_struct)>;
         constexpr uint32_t struct_size = sizeof(struct_type);
 
         // member-offset within struct in bytes
@@ -70,9 +69,9 @@ auto create_handle_pointer_member_function = [](uint8_t* out_data, uint32_t i, u
         // re-direct pointers to point at copy
         if (out_data != nullptr)
         {
-            using pointer_type = std::decay_t<decltype(pointer_member)>;
+            using pointer_type    = std::decay_t<decltype(pointer_member)>;
             pointer_type* out_ptr = reinterpret_cast<pointer_type*>(out_data + i * struct_size + member_offset);
-            *out_ptr = reinterpret_cast<pointer_type>(offset_ptr(out_data, offset));
+            *out_ptr              = reinterpret_cast<pointer_type>(offset_ptr(out_data, offset));
         }
         offset += copy_size;
     };
@@ -131,39 +130,42 @@ size_t deep_copy(const VkPipelineVertexInputStateCreateInfo* structs, uint32_t c
 template <>
 size_t deep_copy(const VkGraphicsPipelineCreateInfo* structs, uint32_t count, uint8_t* out_data)
 {
-    uint64_t offset   = 0;
+    using struct_type              = std::decay_t<decltype(*structs)>;
+    constexpr uint32_t struct_size = sizeof(struct_type);
+
+    uint64_t offset = 0;
 
     // start with base structure array
-    offset += sizeof(VkGraphicsPipelineCreateInfo) * count;
+    offset += struct_size * count;
 
     for (uint32_t i = 0; i < count; ++i)
     {
-        const auto& create_info = structs[i];
+        const auto& base_struct = structs[i];
 
         if (out_data != nullptr)
         {
-            auto out_structures = reinterpret_cast<VkGraphicsPipelineCreateInfo*>(out_data);
-            out_structures[i]   = create_info;
+            auto out_structures = reinterpret_cast<struct_type*>(out_data);
+            out_structures[i]   = base_struct;
         }
 
         // copy pNext-chain
-        offset += deep_copy_pnext(create_info.pNext, offset_ptr(out_data, offset));
+        offset += deep_copy_pnext(base_struct.pNext, offset_ptr(out_data, offset));
 
         // create lambda to handle pointer-members
-        auto handle_pointer_member = create_handle_pointer_member_function(out_data, i, offset, create_info);
+        auto handle_pointer_member = create_handle_pointer_member_function(out_data, i, offset, base_struct);
 
         // deep copy all pointer members
-        handle_pointer_member(create_info.pStages, create_info.stageCount);
-        handle_pointer_member(create_info.pVertexInputState, 1);
-        handle_pointer_member(create_info.pInputAssemblyState, 1);
-        handle_pointer_member(create_info.pTessellationState, 1);
-        handle_pointer_member(create_info.pViewportState, 1);
-        handle_pointer_member(create_info.pRasterizationState, 1);
-        handle_pointer_member(create_info.pMultisampleState, 1);
-        handle_pointer_member(create_info.pRasterizationState, 1);
-        handle_pointer_member(create_info.pDepthStencilState, 1);
-        handle_pointer_member(create_info.pColorBlendState, 1);
-        handle_pointer_member(create_info.pDynamicState, 1);
+        handle_pointer_member(base_struct.pStages, base_struct.stageCount);
+        handle_pointer_member(base_struct.pVertexInputState, 1);
+        handle_pointer_member(base_struct.pInputAssemblyState, 1);
+        handle_pointer_member(base_struct.pTessellationState, 1);
+        handle_pointer_member(base_struct.pViewportState, 1);
+        handle_pointer_member(base_struct.pRasterizationState, 1);
+        handle_pointer_member(base_struct.pMultisampleState, 1);
+        handle_pointer_member(base_struct.pRasterizationState, 1);
+        handle_pointer_member(base_struct.pDepthStencilState, 1);
+        handle_pointer_member(base_struct.pColorBlendState, 1);
+        handle_pointer_member(base_struct.pDynamicState, 1);
     }
     return offset;
 }
