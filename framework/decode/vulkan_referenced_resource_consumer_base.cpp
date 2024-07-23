9a9e5cb4808c772e49e3e917b5dd4682e5f8de16
Nick Driscoll <nick@lunarg.com>
2024-07-23 09:58:58 -0400

    Figured out clang-format

--- framework/decode/vulkan_referenced_resource_consumer_base.cpp
+++ framework/decode/vulkan_referenced_resource_consumer_base.cpp
@@ -664,8 +664,8 @@ void VulkanReferencedResourceConsumerBase::Process_vkCmdPushDescriptorSetWithTem
     format::HandleId                                                      commandBuffer,
     StructPointerDecoder<Decoded_VkPushDescriptorSetWithTemplateInfoKHR>* pPushDescriptorSetWithTemplateInfo)
 {
-    Decoded_VkPushDescriptorSetWithTemplateInfoKHR* info                 = pPushDescriptorSetWithTemplateInfo->GetMetaStructPointer();
-    format::HandleId descriptorUpdateTemplate =                 info->descriptorUpdateTemplate;
+    Decoded_VkPushDescriptorSetWithTemplateInfoKHR* info = pPushDescriptorSetWithTemplateInfo->GetMetaStructPointer();
+    format::HandleId                                descriptorUpdateTemplate = info->descriptorUpdateTemplate;
     DescriptorUpdateTemplateDecoder                 pData                    = info->pData;
 
     PushDescriptorSetWithTemplate(commandBuffer, descriptorUpdateTemplate, &pData);
