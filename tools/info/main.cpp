645914161b28ff5f0057483ec46f0853e174fc4d
Rodrigo Urra <rodrigo.urra@amd.com>
2024-10-24 10:49:47 -0400

    Update copyright

--- tools/info/main.cpp
+++ tools/info/main.cpp
@@ -1,6 +1,6 @@
 /*
 ** Copyright (c) 2020-2024 LunarG, Inc.
-** Copyright (c) 2022-2023 Advanced Micro Devices, Inc. All rights reserved.
+** Copyright (c) 2022-2024 Advanced Micro Devices, Inc. All rights reserved.
 **
 ** Permission is hereby granted, free of charge, to any person obtaining a
 ** copy of this software and associated documentation files (the "Software"),
