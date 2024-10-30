487b907ba56dbe8d1a96868d7ad982ec17ffa7f2
Mark Young <marky@lunarg.com>
2024-10-30 10:49:20 -0400

    codegen: Update khronos base to have a write
    Since the khronos_base files were referring back to the parent
    "base_generator" (which in this case was Vulkan) it still resulted
    in a tight coupling.  Move the "write" method into the
    khronos_base_generator script to solve this.

--- framework/generated/khronos_generators/khronos_base_replay_consumer_body_generator.py
+++ framework/generated/khronos_generators/khronos_base_replay_consumer_body_generator.py
@@ -21,7 +21,7 @@
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 # IN THE SOFTWARE.
 
-from base_generator import write
+from khronos_base_generator import write
 
 
 class KhronosBaseReplayConsumerBodyGenerator():
