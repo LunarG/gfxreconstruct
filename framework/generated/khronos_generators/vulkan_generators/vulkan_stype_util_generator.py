b8b4fb760f5c1acf434386a0c680dad4e43023b8
Mark Young <marky@lunarg.com>
2024-10-31 18:32:57 -0400

    codegen: Move struct type gen into Khronos
    Move the structure type util generation into the Khronos base.

--- framework/generated/khronos_generators/vulkan_generators/vulkan_stype_util_generator.py
+++ framework/generated/khronos_generators/vulkan_generators/vulkan_stype_util_generator.py
@@ -21,11 +21,12 @@
 # IN THE SOFTWARE.
 
 from base_generator import BaseGenerator, BaseGeneratorOptions, write
+from khronos_struct_type_util_generator import KhronosStructTypeUtilGenerator
 from reformat_code import format_cpp_code
 
 VulkanSTypeUtilGeneratorOptions = BaseGeneratorOptions
 
-class VulkanSTypeUtilGenerator(BaseGenerator):
+class VulkanSTypeUtilGenerator(BaseGenerator, KhronosStructTypeUtilGenerator):
     """VulkanSTypeUtilGenerator - subclass of BaseGenerator.
     Generates C++ utility header to do compile-time lookups between Vulkan
     structure types and their corresponding VkStructureType values
@@ -38,42 +39,10 @@ class VulkanSTypeUtilGenerator(BaseGenerator):
 
     def beginFile(self, gen_opts):
         BaseGenerator.beginFile(self, gen_opts)
-        write('#include "format/platform_types.h"', file=self.outFile)
-        write('#include "util/defines.h"', file=self.outFile)
-        self.newline()
-        self.writeIncludesOfCommonApiHeaders(gen_opts)
-        self.newline()
-
-        write('GFXRECON_BEGIN_NAMESPACE(gfxrecon)', file=self.outFile)
-        write('GFXRECON_BEGIN_NAMESPACE(util)', file=self.outFile)
-        self.newline()
-
-        current_api_data = self.getApiData()
-        write('// Instantiating the primary template indicates that either the template was', file=self.outFile)
-        write('// called with an invalid {} struct type or that the code generation is out'.format(
-            current_api_data.api_name
-        ), file=self.outFile)
-        write('// of date, both of which are errors', file=self.outFile)
-        write('template <typename T> {} Get{}() = delete;'.format(
-                current_api_data.struct_type_enum,
-                current_api_data.struct_type_func_prefix
-        ), file=self.outFile)
-        self.newline()
+        KhronosStructTypeUtilGenerator.writeStructTypePrefix(self, gen_opts)
 
     def endFile(self):
-        current_api_data = self.getApiData()
-        for struct in self.all_struct_members.keys():
-            if struct in self.struct_type_names:
-                write('template <> constexpr {} Get{}<{}>(){{ return {}; }}'.format(
-                    current_api_data.struct_type_enum,
-                    current_api_data.struct_type_func_prefix,
-                    struct,
-                    self.struct_type_names[struct]
-                ), file=self.outFile)
-
-        self.newline()
-        write('GFXRECON_END_NAMESPACE(util)', file=self.outFile)
-        write('GFXRECON_END_NAMESPACE(gfxrecon)', file=self.outFile)
+        KhronosStructTypeUtilGenerator.writeStructTypeData(self)
 
         # Finish processing in superclass
         BaseGenerator.endFile(self)
