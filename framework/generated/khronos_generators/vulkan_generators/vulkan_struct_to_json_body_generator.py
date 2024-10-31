b1e5945a90c88ac301ff509f8df74be9b78a4721
Mark Young <marky@lunarg.com>
2024-10-31 11:08:24 -0400

    codegen: Remove process_* & feature_break
    Remove now unused "process_structs", "process_cmds", and "feature_break"
    variables which were not always used correctly.
    
    NOTE: No generated code was changed after running scripts.

--- framework/generated/khronos_generators/vulkan_generators/vulkan_struct_to_json_body_generator.py
+++ framework/generated/khronos_generators/vulkan_generators/vulkan_struct_to_json_body_generator.py
@@ -62,9 +62,6 @@ class VulkanStructToJsonBodyGenerator(BaseGenerator):
     ):
         BaseGenerator.__init__(
             self,
-            process_cmds=True,
-            process_structs=True,
-            feature_break=False,
             err_file=err_file,
             warn_file=warn_file,
             diag_file=diag_file
@@ -149,7 +146,6 @@ class VulkanStructToJsonBodyGenerator(BaseGenerator):
     #
     # Indicates that the current feature has C++ code to generate.
     def need_feature_generation(self):
-        self.feature_break = False
         if self.feature_struct_members:
             return True
         return False
