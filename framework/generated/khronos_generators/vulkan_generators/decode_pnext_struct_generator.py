75f12aabe62f3a312738085273273445e1348e0c
Mark Young <marky@lunarg.com>
2024-11-11 17:31:52 -0500

    codegen: Move decode pnext into khronos
    Move the actual methods for generating the decode of the pnext items
    into a khronos generic file.

--- framework/generated/khronos_generators/vulkan_generators/decode_pnext_struct_generator.py
+++ framework/generated/khronos_generators/vulkan_generators/decode_pnext_struct_generator.py
@@ -23,6 +23,7 @@
 
 import sys
 from base_generator import BaseGenerator, BaseGeneratorOptions, write
+from khronos_decode_extended_struct_generator import KhronosDecodeExtendedStructGenerator
 
 
 class DecodePNextStructGeneratorOptions(BaseGeneratorOptions):
@@ -53,7 +54,7 @@ class DecodePNextStructGeneratorOptions(BaseGeneratorOptions):
         )
 
 
-class DecodePNextStructGenerator(BaseGenerator):
+class DecodePNextStructGenerator(BaseGenerator, KhronosDecodeExtendedStructGenerator):
     """DecodePNextStructGenerator - subclass of BaseGenerator.
     Generates C++ code for Vulkan API pNext structure decoding.
     Generate pNext structure decoding C++ code.
@@ -73,193 +74,25 @@ class DecodePNextStructGenerator(BaseGenerator):
         """Method override."""
         BaseGenerator.beginFile(self, gen_opts)
 
-        self.writeCommonHeaders(gen_opts)
+        KhronosDecodeExtendedStructGenerator.writeCommonHeaders(self, gen_opts)
 
         self.newline()
         write('GFXRECON_BEGIN_NAMESPACE(gfxrecon)', file=self.outFile)
         write('GFXRECON_BEGIN_NAMESPACE(decode)', file=self.outFile)
         self.newline()
 
-        self.writeDecodeStructDefinitionPrefix()
-
-    def writeCommonHeaders(self, gen_opts):
-        # Get the current API and generate the items relavent to that
-        current_api_data = self.getApiData()
-        lower_api_name = current_api_data.api_name.lower()
-
-        write(
-            '#include "decode/custom_{}_struct_decoders.h"'.format(lower_api_name),
-            file=self.outFile
-        )
-        write('#include "decode/decode_allocator.h"', file=self.outFile)
-        write('#include "decode/{}_{}_node.h"'.format(
-                lower_api_name,
-                current_api_data.extended_struct_variable.lower()),
-            file=self.outFile)
-        write('#include "decode/{}_{}_typed_node.h"'.format(
-                lower_api_name,
-                current_api_data.extended_struct_variable.lower()), file=self.outFile)
-        write(
-            '#include "generated/generated_{}_struct_decoders.h"'.format(
-                lower_api_name),file=self.outFile)
-        write(
-            '#include "generated/generated_{}_enum_to_string.h"'.format(
-            lower_api_name), file=self.outFile)
-        write('#include "util/logging.h"', file=self.outFile)
-        self.newline()
-        write('#include <cassert>', file=self.outFile)
-
-    def writeDecodeStructDefinitionPrefix(self):
-        current_api_data = self.getApiData()
-
-        write(
-            'size_t Decode{prefix}Struct(const uint8_t* parameter_buffer, size_t buffer_size, {prefix}Node** {})'.format(
-                current_api_data.extended_struct_variable,
-                prefix=current_api_data.extended_struct_func_prefix
-            ),
-            file=self.outFile
-        )
-        write('{', file=self.outFile)
-        write('    assert({} != nullptr);'.format(
-                current_api_data.extended_struct_variable,
-            ), file=self.outFile)
-        self.newline()
-        write('    size_t bytes_read = 0;', file=self.outFile)
-        write('    uint32_t attrib = 0;', file=self.outFile)
-        self.newline()
-
-        offset_var = current_api_data.struct_type_variable.lower() + '_offset'
-        write(
-            '    if ((parameter_buffer != nullptr) && (buffer_size >= sizeof(attrib)))',
-            file=self.outFile
-        )
-        write('    {', file=self.outFile)
-        write('        size_t {} = 0;'.format(offset_var), file=self.outFile)
-        self.newline()
-        write(
-            '        // Peek at the pointer attribute mask to make sure we have a non-NULL value that can be decoded.',
-            file=self.outFile
-        )
-        write(
-            '        attrib = *(reinterpret_cast<const uint32_t*>(parameter_buffer));',
-            file=self.outFile
-        )
-        self.newline()
-        write(
-            '        if ((attrib & format::PointerAttributes::kIsNull) != format::PointerAttributes::kIsNull)',
-            file=self.outFile
-        )
-        write('        {', file=self.outFile)
-        write(
-            '            // Offset to {}, after the pointer encoding preamble.'.format(current_api_data.struct_type_enum),
-            file=self.outFile
-        )
-        write('            {} = sizeof(attrib);'.format(offset_var), file=self.outFile)
-        self.newline()
-        write(
-            '            if ((attrib & format::PointerAttributes::kHasAddress) == format::PointerAttributes::kHasAddress)',
-            file=self.outFile
-        )
-        write('            {', file=self.outFile)
-        write(
-            '                {} += sizeof(format::AddressEncodeType);'.format(offset_var),
-            file=self.outFile
-        )
-        write('            }', file=self.outFile)
-        write('        }', file=self.outFile)
-        self.newline()
-        write(
-            '        if (({offset} != 0) && ((buffer_size - {offset}) >= sizeof({})))'.format(
-                current_api_data.struct_type_enum,
-                offset=offset_var
-            ),
-            file=self.outFile
-        )
-        write('        {', file=self.outFile)
-        write(
-            '            const {struct_type}* {} = reinterpret_cast<const {struct_type}*>(parameter_buffer + {});'.format(
-                current_api_data.struct_type_variable,
-                offset_var,
-                struct_type=current_api_data.struct_type_enum
-            ),
-            file=self.outFile
-        )
-        self.newline()
-        write('            switch (*{})'.format(current_api_data.struct_type_variable), file=self.outFile)
-        write('            {', file=self.outFile)
-        write('            default:', file=self.outFile)
-        write(
-            '                // TODO: This may need to be a fatal error',
-            file=self.outFile
-        )
-        write(
-            '                GFXRECON_LOG_ERROR("Failed to decode {} value with unrecognized {} = %s", (util::ToString(*{}).c_str()));'.format(
-                current_api_data.extended_struct_variable,
-                current_api_data.struct_type_enum,
-                current_api_data.struct_type_variable,
-            ),
-            file=self.outFile
-        )
-        write('                break;', file=self.outFile)
+        KhronosDecodeExtendedStructGenerator.writeDecodeStructDefinitionPrefix(self)
 
     def endFile(self):
         """Method override."""
         """Performs C++ code generation for the feature."""
-        self.writeDecodeStructDefinitionData()
+        KhronosDecodeExtendedStructGenerator.writeDecodeStructDefinitionData(self)
         self.newline()
         write('GFXRECON_END_NAMESPACE(decode)', file=self.outFile)
         write('GFXRECON_END_NAMESPACE(gfxrecon)', file=self.outFile)
 
         # Finish processing in superclass
         BaseGenerator.endFile(self)
-
-    def writeDecodeStructDefinitionData(self):
-        current_api_data = self.getApiData()
-
-        for struct in self.all_extended_structs:
-            if struct in self.struct_type_names:
-                stype = self.struct_type_names[struct]
-                write(
-                    '            case {}:'.format(stype),
-                    file=self.outFile
-                )
-                write(
-                    '                (*{}) = DecodeAllocator::Allocate<{}TypedNode<Decoded_{}>>();'
-                    .format(
-                        current_api_data.extended_struct_variable,
-                        current_api_data.extended_struct_func_prefix,
-                        struct),
-                    file=self.outFile
-                )
-                write(
-                    '                bytes_read = (*{})->Decode(parameter_buffer, buffer_size);'
-                    .format(
-                        current_api_data.extended_struct_variable
-                    ),
-                    file=self.outFile
-                )
-                write('                break;', file=self.outFile)
-
-        write('            }', file=self.outFile)
-        write('        }', file=self.outFile)
-        write('    }', file=self.outFile)
-        self.newline()
-        write('    if ((bytes_read == 0) && (attrib != 0))', file=self.outFile)
-        write('    {', file=self.outFile)
-        write(
-            '        // The encoded pointer attribute mask included kIsNull, or the {} was unrecognized.'
-            .format(current_api_data.struct_type_variable),
-            file=self.outFile
-        )
-        write(
-            '        // We will report that we read the attribute mask, but nothing else was decoded.',
-            file=self.outFile
-        )
-        write('        bytes_read = sizeof(attrib);', file=self.outFile)
-        write('    }', file=self.outFile)
-        self.newline()
-        write('    return bytes_read;', file=self.outFile)
-        write('}', file=self.outFile)
 
     def need_feature_generation(self):
         """Indicates that the current feature has C++ code to generate."""
