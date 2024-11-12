80c450da279cbf98f0a9f33866b1764e7c7c2805
Mark Young <marky@lunarg.com>
2024-11-12 11:07:55 -0500

    codegen: Add parent->child to struct->json header

--- framework/generated/khronos_generators/khronos_struct_to_json_header_generator.py
+++ framework/generated/khronos_generators/khronos_struct_to_json_header_generator.py
@@ -32,12 +32,45 @@ class KhronosStructToJsonHeaderGenerator():
         Indicates that the provided struct is a struct we want to decode"""
         return True
 
+    def write_parent_child_to_json_func(self):
+        struct_type = self.get_struct_type_enum_name()
+        body = '\n'
+        body += 'template <typename T>\n'
+        body += 'void ParentChildFieldToJson(nlohmann::ordered_json& jdata, const T* data, const util::JsonOptions& options = util::JsonOptions())\n'
+        body += '{\n'
+        body += '    // First read in the type to know which child we need to handle\n'
+        body += f'    {struct_type} struct_type;\n'
+        body += '    FieldToJson(jdata["type"], struct_type, options);\n'
+        body += '\n'
+        body += '    switch (struct_type)\n'
+        body += '    {\n'
+        body += '        default:\n'
+        body += '        {\n'
+        body += '            GFXRECON_LOG_WARNING("ParentChildFieldToJson: unrecognized child structure type %d", struct_type);\n'
+        body += '            break;\n'
+        body += '        }\n'
+        for child_list in self.children_structs:
+            for child in self.children_structs[child_list]:
+                struct_type_name = self.struct_type_names[child]
+                body += f'        case {struct_type_name}:\n'
+                body += '        {\n'
+                body += f'            FieldToJson(jdata, reinterpret_cast<const Decoded_{child}*>(data), options);\n'
+                body += '            break;\n'
+                body += '        }\n'
+        body += '    }\n'
+        body += '}\n'
+        write(body, file=self.outFile)
+
     def write_header_contents(self):
         for struct in self.get_all_filtered_struct_names():
             if self.should_decode_struct(struct):
                 body = "void FieldToJson(nlohmann::ordered_json& jdata, const Decoded_{0}* data, const util::JsonOptions& options = util::JsonOptions());".format(struct)
                 write(body, file=self.outFile)
         self.newline()
+
+        # If there are parent/child structs, write the utility function to help
+        if len(self.children_structs) > 0:
+            self.write_parent_child_to_json_func()
 
         var_name = self.get_extended_struct_var_name()
         prefix = self.get_extended_struct_func_prefix()
