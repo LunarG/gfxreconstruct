5796715b801305f1f1fdb1f34b7e074a57c40233
Mark Young <marky@lunarg.com>
2024-11-14 16:28:57 -0500

    codegen: Refactor state table gen
    Use apidata for state table header class
    Move struct table header gen to khronos

--- framework/generated/khronos_generators/vulkan_generators/vulkan_state_table_header_generator.py
+++ framework/generated/khronos_generators/vulkan_generators/vulkan_state_table_header_generator.py
@@ -22,6 +22,7 @@
 
 import os, re, sys
 from base_generator import *
+from khronos_state_table_header_generator import KhronosStateTableHeaderGenerator
 
 
 class VulkanStateTableHeaderGeneratorOptions(BaseGeneratorOptions):
@@ -52,7 +53,7 @@ class VulkanStateTableHeaderGeneratorOptions(BaseGeneratorOptions):
 
 
 # Generates declarations for functions for Vulkan state table
-class VulkanStateTableHeaderGenerator(BaseGenerator):
+class VulkanStateTableHeaderGenerator(BaseGenerator, KhronosStateTableHeaderGenerator):
 
     def __init__(
         self, err_file=sys.stderr, warn_file=sys.stderr, diag_file=sys.stdout
@@ -68,7 +69,10 @@ class VulkanStateTableHeaderGenerator(BaseGenerator):
     # yapf: disable
     def beginFile(self, genOpts):
         BaseGenerator.beginFile(self, genOpts)
-        self.write_include()
+        write(
+            '#include "encode/vulkan_state_table_base.h"\n', file=self.outFile
+        )
+        self.newline()
         write('GFXRECON_BEGIN_NAMESPACE(gfxrecon)', file=self.outFile)
         write('GFXRECON_BEGIN_NAMESPACE(encode)', file=self.outFile)
     # yapf: enable
@@ -76,111 +80,11 @@ class VulkanStateTableHeaderGenerator(BaseGenerator):
     # Method override
     # yapf: disable
     def endFile(self):
-        self.generate_all()
+        KhronosStateTableHeaderGenerator.generate_state_table_content(self)
+
         write('GFXRECON_END_NAMESPACE(encode)', file=self.outFile)
         write('GFXRECON_END_NAMESPACE(gfxrecon)', file=self.outFile)
 
         # Finish processing in superclass
         BaseGenerator.endFile(self)
     # yapf: enable
-
-    def add_wrapper_funcs_for_type(self, name):
-        wrapper_prefix = self.get_wrapper_prefix_from_type(name)
-        type_prefix = self.get_api_struct_prefix_from_type(name)
-        short_name = name[len(type_prefix):]
-        handle_wrapper_func = short_name + 'Wrapper'
-        handle_wrapper_type = wrapper_prefix + '::' + short_name + 'Wrapper'
-        handle_map = short_name[0].lower() + short_name[1:] + '_map_'
-        self.insert_code += '    bool InsertWrapper(format::HandleId id, {}* wrapper) {{ return InsertEntry(id, wrapper, {}); }}\n'.format(handle_wrapper_type, handle_map)
-        self.remove_code += '    bool RemoveWrapper(const {}* wrapper) {{ return RemoveEntry(wrapper, {}); }}\n'.format(handle_wrapper_type, handle_map)
-        self.visit_code += '    void VisitWrappers(std::function<void({}*)> visitor) const {{ for (auto entry : {}) {{ visitor(entry.second); }} }}\n'.format(handle_wrapper_type, handle_map)
-        self.get_code += '    {0}* Get{1}(format::HandleId id) {{ return GetWrapper<{0}>(id, {2}); }}\n'.format(handle_wrapper_type, handle_wrapper_func, handle_map)
-        self.const_get_code += '    const {0}* Get{1}(format::HandleId id) const {{ return GetWrapper<{0}>(id, {2}); }}\n'.format(handle_wrapper_type, handle_wrapper_func, handle_map)
-        self.map_code += '    std::map<format::HandleId, {}*> {};\n'.format(handle_wrapper_type, handle_map)
-        self.api_insert_code += '    bool InsertWrapper({}* wrapper) {{ return InsertEntry(wrapper->handle, wrapper, {}); }}\n'.format(handle_wrapper_type, handle_map)
-        self.api_remove_code += '    bool RemoveWrapper(const {}* wrapper) {{\n'.format(handle_wrapper_type)
-        self.api_remove_code += '         if (wrapper == nullptr) return false;\n'
-        self.api_remove_code += '         return RemoveEntry(wrapper->handle, {});\n'.format(handle_map)
-        self.api_remove_code += '    }\n'
-        self.api_get_code += 'template<> inline {0}* VulkanStateHandleTable::GetWrapper<{0}>({1} handle) {{ return VulkanStateTableBase::GetWrapper(handle, {2}); }}\n'.format(handle_wrapper_type, name, handle_map)
-        self.api_const_get_code += 'template<> inline const {0}* VulkanStateHandleTable::GetWrapper<{0}>({1} handle) const {{ return VulkanStateTableBase::GetWrapper(handle, {2}); }}\n'.format(handle_wrapper_type, name, handle_map)
-        self.api_map_code += '    std::unordered_map<{}, {}*> {};\n'.format(name, handle_wrapper_type, handle_map)
-
-    # yapf: disable
-    def generate_all(self):
-        self.insert_code = ''
-        self.remove_code = ''
-        self.const_get_code = ''
-        self.get_code = ''
-        self.visit_code = ''
-        self.map_code = ''
-
-        self.api_insert_code = ''
-        self.api_remove_code = ''
-        self.api_const_get_code = ''
-        self.api_get_code = ''
-        self.api_map_code = ''
-
-        for handle in sorted(self.handle_names):
-            if handle in self.handle_aliases:
-                continue
-            self.add_wrapper_funcs_for_type(handle)
-        for atom in sorted(self.atom_names):
-            if atom in self.atom_aliases:
-                continue
-            self.add_wrapper_funcs_for_type(atom)
-        for opaque in sorted(self.opaque_names):
-            if atom in self.opaque_aliases:
-                continue
-            self.add_wrapper_funcs_for_type(opaque)
-
-        self.newline()
-        code = 'class VulkanStateTable : VulkanStateTableBase\n'
-        code += '{\n'
-        code += '  public:\n'
-        code += '    VulkanStateTable() {}\n'
-        code += '    ~VulkanStateTable() {}\n'
-        code += '\n'
-        code += self.insert_code
-        code += '\n'
-        code += self.remove_code
-        code += '\n'
-        code += self.const_get_code
-        code += '\n'
-        code += self.get_code
-        code += '\n'
-        code += self.visit_code
-        code += '\n'
-        code += '  private:\n'
-        code += self.map_code
-        code += '};\n'
-        code += '\n'
-        code += 'class VulkanStateHandleTable : VulkanStateTableBase\n'
-        code += '{\n'
-        code += '  public:\n'
-        code += '    VulkanStateHandleTable() {}\n'
-        code += '    ~VulkanStateHandleTable() {}\n'
-        code += '\n'
-        code += self.api_insert_code
-        code += '\n'
-        code += self.api_remove_code
-        code += '\n'
-        code += '    template<typename Wrapper> const Wrapper* GetWrapper(typename Wrapper::HandleType handle) const { return nullptr; }\n'
-        code += '\n'
-        code += '    template<typename Wrapper> Wrapper* GetWrapper(typename Wrapper::HandleType handle) { return nullptr; }\n'
-        code += '\n'                
-        code += '  private:\n'
-        code += self.api_map_code
-        code += '};\n'
-        code += '\n'
-        code += self.api_const_get_code
-        code += '\n'
-        code += self.api_get_code
-        write(code, file=self.outFile)
-    # yapf: enable
-
-    def write_include(self):
-        write(
-            '#include "encode/vulkan_state_table_base.h"\n', file=self.outFile
-        )
-        self.newline()
