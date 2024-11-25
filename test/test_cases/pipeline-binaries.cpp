07bb611efdfc7f68c11b8639a626504f4a05e120
beau-lunarg <beau@lunarg.com>
2024-11-25 14:14:57 -0800

    test cases for shader-objects and pipeline-binaries test apps

---
+++ test/test_cases/pipeline-binaries.cpp
@@ -0,0 +1,13 @@
+#include <gtest/gtest.h>
+
+#include "verify-gfxr.h"
+
+char const* const APP_RUN_DIRECTORY = "pipeline-binaries";
+char const* const APP_PATH          = "Debug/gfxrecon-testapp-pipeline-binaries.exe";
+char const* const CAPTURE_FILE      = "pipeline-binaries.gfxr";
+char const* const KNOWN_GFXR_PATH   = "pipeline-binaries.gfxr";
+
+TEST(PipelineBinaries, CorrectGFXR)
+{
+    verify_gfxr(APP_RUN_DIRECTORY, APP_PATH, CAPTURE_FILE, KNOWN_GFXR_PATH);
+}
