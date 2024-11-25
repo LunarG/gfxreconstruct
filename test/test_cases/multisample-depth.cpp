f1cde7a8c18e7ac5e5e1be388867fc06491fb9c5
beau-lunarg <beau@lunarg.com>
2024-11-25 14:13:21 -0800

    Multisample depth test case

---
+++ test/test_cases/multisample-depth.cpp
@@ -0,0 +1,13 @@
+#include <gtest/gtest.h>
+
+#include "verify-gfxr.h"
+
+char const* const APP_RUN_DIRECTORY = "multisample-depth";
+char const* const APP_PATH          = "Debug/gfxrecon-testapp-multisample-depth.exe";
+char const* const CAPTURE_FILE      = "multisample-depth.gfxr";
+char const* const KNOWN_GFXR_PATH   = "multisample-depth.gfxr";
+
+TEST(MultisampleDepth, CorrectGFXR)
+{
+    verify_gfxr(APP_RUN_DIRECTORY, APP_PATH, CAPTURE_FILE, KNOWN_GFXR_PATH);
+}
