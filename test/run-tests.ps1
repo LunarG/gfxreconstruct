d942966a3c90c348a0345f95bca65d39f84c0406
beau-lunarg <beau@lunarg.com>
2024-12-02 10:02:07 -0500

    GFXRECON log level info to debug CI issue

--- test/run-tests.ps1
+++ test/run-tests.ps1
@@ -16,5 +16,6 @@ if ($is_admin) {
 }
 $env:GFXRECON_CAPTURE_FILE_TIMESTAMP="false"
 $env:GFXRECON_CAPTURE_FILE="actual.gfxr"
+$env:GFXRECON_LOG_LEVEL="info"
 $env:VK_LOADER_DEBUG="info"
 ./gfxrecon-testapp-runner.exe
