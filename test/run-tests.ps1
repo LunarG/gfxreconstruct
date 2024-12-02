86da35223d1feeef1fa384678b764194d1ee77a4
beau-lunarg <beau@lunarg.com>
2024-12-02 10:21:54 -0500

    Loader debug layer, fix disable_environment

--- test/run-tests.ps1
+++ test/run-tests.ps1
@@ -17,5 +17,5 @@ if ($is_admin) {
 $env:GFXRECON_CAPTURE_FILE_TIMESTAMP="false"
 $env:GFXRECON_CAPTURE_FILE="actual.gfxr"
 $env:GFXRECON_LOG_LEVEL="info"
-$env:VK_LOADER_DEBUG="info"
+$env:VK_LOADER_DEBUG="layer"
 ./gfxrecon-testapp-runner.exe
