aba159d772dae2c4f9d0c3b3eadbb0eeb5b59b50
beau-lunarg <beau@lunarg.com>
2024-12-11 10:20:19 -0500

    Add message when fetching sdl3

--- cmake/FindSDL3.cmake
+++ cmake/FindSDL3.cmake
@@ -1,7 +1,10 @@
 include(FetchContent)
+
+set(SDL_GIT "https://github.com/libsdl-org/SDL.git")
+message(STATUS "Fetching SDL2 files from ${SDL_GIT}")
 FetchContent_Declare(
         sdl
-        GIT_REPOSITORY https://github.com/libsdl-org/SDL.git
+        GIT_REPOSITORY ${SDL_GIT}
         GIT_TAG preview-3.1.3
 )
 FetchContent_MakeAvailable(sdl)
