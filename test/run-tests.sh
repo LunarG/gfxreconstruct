9cafef946b67cc32544ab10774c88b66d9dd24a1
beau-lunarg <beau@lunarg.com>
2024-12-18 12:06:26 -0500

    Use set instead of export

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -1,10 +1,10 @@
-export VK_LAYER_PATH="$PWD/vulkan/explicit_layer.d"
-export VK_DRIVER_FILES="$PWD/test_apps/VkICD_mock_icd.json"
-export VK_INSTANCE_LAYERS='VK_LAYER_LUNARG_gfxreconstruct'
-export GFXRECON_CAPTURE_FILE_TIMESTAMP='false'
-export GFXRECON_CAPTURE_FILE='actual.gfxr'
-export VK_LOADER_LAYERS_DISABLE='~all~'
-export VK_LOADER_LAYERS_ENABLE='*gfxreconstruct*'
-export GFXRECON_LOG_LEVEL='info'
-export VK_LOADER_DEBUG='layer'
+set VK_LAYER_PATH="$PWD/vulkan/explicit_layer.d"
+set VK_DRIVER_FILES="$PWD/test_apps/VkICD_mock_icd.json"
+set VK_INSTANCE_LAYERS=VK_LAYER_LUNARG_gfxreconstruct
+set GFXRECON_CAPTURE_FILE_TIMESTAMP=false
+set GFXRECON_CAPTURE_FILE=actual.gfxr
+set VK_LOADER_LAYERS_DISABLE=~all~
+set VK_LOADER_LAYERS_ENABLE=*gfxreconstruct*
+set GFXRECON_LOG_LEVEL=info
+set VK_LOADER_DEBUG=layer
 ./gfxrecon-testapp-runner
