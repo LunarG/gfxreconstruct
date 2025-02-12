3aa1ca6f81f82a4e5ca4447c16fb1dedf5ef54b9
Panagiotis Apostolou <panosa@lunarg.com>
2025-02-12 13:22:21 +0200

    VDR: Fix when dumping images raw

--- framework/decode/vulkan_replay_dump_resources_common.cpp
+++ framework/decode/vulkan_replay_dump_resources_common.cpp
@@ -540,9 +540,6 @@ VkResult DumpImageToFile(const VulkanImageInfo*             image_info,
                                                                            image_file_format,
                                                                            dump_image_raw);
 
-        const util::imagewriter::DataFormats image_writer_format = VkFormatToImageWriterDataFormat(dst_format);
-        assert(image_writer_format != util::imagewriter::DataFormats::kFormat_UNSPECIFIED);
-
         for (uint32_t mip = 0; mip < image_info->level_count; ++mip)
         {
             for (uint32_t layer = 0; layer < image_info->layer_count; ++layer)
@@ -559,6 +556,10 @@ VkResult DumpImageToFile(const VulkanImageInfo*             image_info,
 
                 if (output_image_format != KFormatRaw)
                 {
+                    const util::imagewriter::DataFormats image_writer_format =
+                        VkFormatToImageWriterDataFormat(dst_format);
+                    assert(image_writer_format != util::imagewriter::DataFormats::kFormat_UNSPECIFIED);
+
                     VkExtent3D scaled_extent;
                     if (scale != 1.0f && scaled)
                     {
@@ -625,9 +626,12 @@ VkResult DumpImageToFile(const VulkanImageInfo*             image_info,
                 }
                 else
                 {
-                    GFXRECON_LOG_WARNING(
-                        "%s format is not handled. Images with that format will be dump as a plain binary file.",
-                        util::ToString<VkFormat>(image_info->format).c_str());
+                    if (!dump_image_raw)
+                    {
+                        GFXRECON_LOG_WARNING(
+                            "%s format is not handled. Images with that format will be dump as a plain binary file.",
+                            util::ToString<VkFormat>(image_info->format).c_str());
+                    }
 
                     util::bufferwriter::WriteBuffer(filename, offsetted_data, subresource_sizes[sub_res_idx]);
                 }
