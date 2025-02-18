df53ded9029d0d2375b930b5874f383fd7953c2f
Locke Lin <locke@lunarg.com>
2025-02-18 14:26:14 -0700

    Remove outdated comments.

--- tools/tool_settings.h
+++ tools/tool_settings.h
@@ -1118,10 +1118,6 @@ GetVulkanReplayOptions(const gfxrecon::util::ArgumentParser&           arg_parse
     const std::string& dump_resources = arg_parser.GetArgumentValue(kDumpResourcesArgument);
     if (!dump_resources.empty() && dump_resources.find_first_not_of("0123456789,") == std::string::npos)
     {
-        // If the option parameter does not split into three comma separated numbers, consider
-        // it a Vulkan option and ignore it. If it does split into three comma separated numbers,
-        // the arg is for dx12 and should have already been validated in the Vulkan option parsing.
-        // In that case, we simply extract and save the values here.
         std::vector<std::string> values = gfxrecon::util::strings::SplitString(dump_resources, ',');
         if (values.size() == 3)
         {
@@ -1210,10 +1206,6 @@ static gfxrecon::decode::DxReplayOptions GetDxReplayOptions(const gfxrecon::util
     const std::string& dump_resources = arg_parser.GetArgumentValue(kDumpResourcesArgument);
     if (!dump_resources.empty() && dump_resources.find_first_not_of("0123456789,") == std::string::npos)
     {
-        // If the option parameter does not split into three comma separated numbers, consider
-        // it a Vulkan option and ignore it. If it does split into three comma separated numbers,
-        // the arg is for dx12 and should have already been validated in the Vulkan option parsing.
-        // In that case, we simply extract and save the values here.
         std::vector<std::string> values = gfxrecon::util::strings::SplitString(dump_resources, ',');
         if (values.size() == 3)
         {
