d50354620c3851168a2c510491b636ef149dbdc0
Antonio Caggiano <antonio@lunarg.com>
2025-04-30 16:55:25 +0200

    test: Use launcher in test cases

--- test/test_cases/wait-for-present.cpp
+++ test/test_cases/wait-for-present.cpp
@@ -2,14 +2,6 @@
 
 #include "verify-gfxr.h"
 
-char const* const APP_RUN_DIRECTORY = "wait-for-present";
-
-#ifdef WIN32
-char const* const APP_PATH          = "gfxrecon-testapp-wait-for-present.exe";
-#else
-char const* const APP_PATH          = "gfxrecon-testapp-wait-for-present";
-#endif
-
 #ifdef __APPLE__
 char const* const KNOWN_GFXR_PATH   = "wait-for-present_macos.gfxr";
 #else
@@ -20,5 +12,5 @@ char const* const TRIMMING_FRAMES = "2";
 
 TEST(WaitForPresent, CorrectGFXR)
 {
-    verify_gfxr(APP_RUN_DIRECTORY, APP_PATH, KNOWN_GFXR_PATH, TRIMMING_FRAMES);
+    verify_gfxr("wait-for-present", KNOWN_GFXR_PATH, TRIMMING_FRAMES);
 }
