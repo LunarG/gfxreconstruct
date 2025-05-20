8d94b270ec0ff3410ae1a52f562d3d72b34134f0
Antonio Caggiano <antonio@lunarg.com>
2025-05-20 15:59:03 -0600

    test: Update googletest

--- cmake/FindGoogleTest.cmake
+++ cmake/FindGoogleTest.cmake
@@ -3,6 +3,6 @@ include(FetchContent)
 FetchContent_Declare(
         googletest
         GIT_REPOSITORY https://github.com/google/googletest.git
-        GIT_TAG v1.15.2
+        GIT_TAG v1.17.0
 )
 FetchContent_MakeAvailable(googletest)
