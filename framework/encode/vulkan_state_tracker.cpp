8ff1f07e7c9dbe5b45602a715b058e43db9da7e6
Rodrigo <Rodrigo.Urra@amd.com>
2025-07-09 10:39:00 -0400

    Add missing copyright

--- framework/encode/vulkan_state_tracker.cpp
+++ framework/encode/vulkan_state_tracker.cpp
@@ -1,5 +1,6 @@
 /*
 ** Copyright (c) 2019-2024 LunarG, Inc.
+*  Copyright (c) 2025 Advanced Micro Devices, Inc. All rights reserved.
 **
 ** Permission is hereby granted, free of charge, to any person obtaining a
 ** copy of this software and associated documentation files (the "Software"),
