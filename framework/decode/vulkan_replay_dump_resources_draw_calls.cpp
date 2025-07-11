fad0399ad4e9c3f14aaf1afb4d57f5293bc0eaeb
Panagiotis Apostolou <panosa@lunarg.com>
2025-07-11 08:35:53 +0300

    VDR: Fix copy of count buffer

--- framework/decode/vulkan_replay_dump_resources_draw_calls.cpp
+++ framework/decode/vulkan_replay_dump_resources_draw_calls.cpp
@@ -362,7 +362,7 @@ VkResult DrawCallsDumpingContext::CopyDrawIndirectParameters(DrawCallParams& dc_
 
             VkCommandBuffer cmd_buf = command_buffers_[current_cb_index_];
             device_table_->CmdCopyBuffer(
-                cmd_buf, ic_params.params_buffer_info->handle, ic_params.new_count_buffer, 1, &region);
+                cmd_buf, ic_params.count_buffer_info->handle, ic_params.new_count_buffer, 1, &region);
 
             VkBufferMemoryBarrier buf_barrier;
             buf_barrier.sType               = VK_STRUCTURE_TYPE_BUFFER_MEMORY_BARRIER;
