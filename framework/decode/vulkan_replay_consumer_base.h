67d17d735bfa1bd3c38053cbf1f5a4d530ea15a1
Fabian Schmidt <fabian@lunarg.com>
2025-07-16 10:43:34 +0200

    Add support for vkCmdTraceRaysIndirectKHR
    - straight-forward, only minor differences to existing support for vkCmdTraceRaysKHR

--- framework/decode/vulkan_replay_consumer_base.h
+++ framework/decode/vulkan_replay_consumer_base.h
@@ -1381,6 +1381,15 @@ class VulkanReplayConsumerBase : public VulkanConsumer
                             uint32_t                                                       height,
                             uint32_t                                                       depth);
 
+    void OverrideCmdTraceRaysIndirectKHR(
+        PFN_vkCmdTraceRaysIndirectKHR                                  func,
+        VulkanCommandBufferInfo*                                       command_buffer_info,
+        StructPointerDecoder<Decoded_VkStridedDeviceAddressRegionKHR>* pRaygenShaderBindingTable,
+        StructPointerDecoder<Decoded_VkStridedDeviceAddressRegionKHR>* pMissShaderBindingTable,
+        StructPointerDecoder<Decoded_VkStridedDeviceAddressRegionKHR>* pHitShaderBindingTable,
+        StructPointerDecoder<Decoded_VkStridedDeviceAddressRegionKHR>* pCallableShaderBindingTable,
+        VkDeviceAddress                                                indirectDeviceAddress);
+
     void
     OverrideCmdBeginRenderPass2(PFN_vkCmdBeginRenderPass2                            func,
                                 VulkanCommandBufferInfo*                             command_buffer_info,
