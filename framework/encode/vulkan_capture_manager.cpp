61a8ceee350e2fc645633f61d7c1326b45d991f9
Fabian Schmidt <165773884+fabian-lunarg@users.noreply.github.com>
2025-08-21 12:00:23 +0200

    Fix crash when primary cmd-buf has invalid inheritance-data (#2366)
    - replace Override with encoder-precall, apply correction to pInheritanceInfo there

--- framework/encode/vulkan_capture_manager.cpp
+++ framework/encode/vulkan_capture_manager.cpp
@@ -1749,27 +1749,6 @@ VkResult VulkanCaptureManager::OverrideAllocateCommandBuffers(VkDevice          
     return result;
 }
 
-VkResult VulkanCaptureManager::OverrideBeginCommandBuffer(VkCommandBuffer                 commandBuffer,
-                                                          const VkCommandBufferBeginInfo* pBeginInfo)
-{
-    auto handle_unwrap_memory = VulkanCaptureManager::Get()->GetHandleUnwrapMemory();
-    auto pBeginInfo_unwrapped = vulkan_wrappers::UnwrapStructPtrHandles(pBeginInfo, handle_unwrap_memory);
-
-    auto modified_begin_info = (*pBeginInfo_unwrapped);
-
-    const auto command_buffer_wrapper =
-        vulkan_wrappers::GetWrapper<vulkan_wrappers::CommandBufferWrapper>(commandBuffer);
-
-    // If command buffer level is primary, pInheritanceInfo must be ignored
-    if (command_buffer_wrapper && command_buffer_wrapper->level == VK_COMMAND_BUFFER_LEVEL_PRIMARY &&
-        modified_begin_info.pInheritanceInfo != nullptr)
-    {
-        modified_begin_info.pInheritanceInfo = nullptr;
-    }
-
-    return vulkan_wrappers::GetDeviceTable(commandBuffer)->BeginCommandBuffer(commandBuffer, &modified_begin_info);
-}
-
 void VulkanCaptureManager::ProcessEnumeratePhysicalDevices(VkResult          result,
                                                            VkInstance        instance,
                                                            uint32_t          count,
@@ -3354,6 +3333,19 @@ void VulkanCaptureManager::PreProcess_vkWaitForFences(
     }
 }
 #endif
+
+void VulkanCaptureManager::PreProcess_vkBeginCommandBuffer(VkCommandBuffer                 commandBuffer,
+                                                           const VkCommandBufferBeginInfo* pBeginInfo)
+{
+    const auto* cmd_buffer_wrapper = vulkan_wrappers::GetWrapper<vulkan_wrappers::CommandBufferWrapper>(commandBuffer);
+
+    // If command buffer level is primary, pInheritanceInfo must be ignored
+    if (cmd_buffer_wrapper != nullptr && cmd_buffer_wrapper->level == VK_COMMAND_BUFFER_LEVEL_PRIMARY)
+    {
+        // const_cast to avoid changes to code-gen
+        const_cast<VkCommandBufferBeginInfo*>(pBeginInfo)->pInheritanceInfo = nullptr;
+    }
+}
 
 void VulkanCaptureManager::PostProcess_vkCmdBindPipeline(VkCommandBuffer     commandBuffer,
                                                          VkPipelineBindPoint pipelineBindPoint,
