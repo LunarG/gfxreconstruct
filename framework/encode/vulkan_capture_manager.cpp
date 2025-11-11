36ca0e863ca18cc766d6fc596c199af1e93d222d
davidd-lunarg <73848817+davidd-lunarg@users.noreply.github.com>
2025-11-10 17:20:46 -0700

    Keep mapped memory ptrs for --capture-copy-data
    Write the originally captured mapped memory pointer values to the trim
    state block when recapturing with --capture-copy-data. This is required
    to ensure mapped memory pointers in data copied from the source capture
    are handled correctly.

--- framework/encode/vulkan_capture_manager.cpp
+++ framework/encode/vulkan_capture_manager.cpp
@@ -2651,6 +2651,16 @@ void VulkanCaptureManager::PreProcess_vkFlushMappedMemoryRanges(VkDevice        
     }
 }
 
+void VulkanCaptureManager::SetOriginalMappedMemoryPointer(VkDeviceMemory memory, const void* original_ptr)
+{
+    auto wrapper = vulkan_wrappers::GetWrapper<vulkan_wrappers::DeviceMemoryWrapper>(memory);
+    assert(wrapper != nullptr);
+    if (wrapper->mapped_data != nullptr)
+    {
+        wrapper->original_mapped_data = original_ptr;
+    }
+}
+
 void VulkanCaptureManager::PreProcess_vkUnmapMemory(VkDevice device, VkDeviceMemory memory)
 {
     auto wrapper = vulkan_wrappers::GetWrapper<vulkan_wrappers::DeviceMemoryWrapper>(memory);
@@ -2665,6 +2675,7 @@ void VulkanCaptureManager::PreProcess_vkUnmapMemory(VkDevice device, VkDeviceMem
             GFXRECON_ASSERT(state_tracker_ != nullptr);
             state_tracker_->TrackAssetsInMemory(wrapper->handle_id);
         }
+        wrapper->original_mapped_data = nullptr;
 
         if (GetMemoryTrackingMode() == CaptureSettings::MemoryTrackingMode::kPageGuard ||
             GetMemoryTrackingMode() == CaptureSettings::MemoryTrackingMode::kUserfaultfd)
