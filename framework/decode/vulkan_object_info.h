bafe116cc923ab1b65ed8278cfef92ad4e34c64e
Fabian Schmidt <165773884+fabian-lunarg@users.noreply.github.com>
2025-11-20 09:05:12 +0100

    Fix issues around AS track&replace (-m rebind) (#2520)
    Vulkan acceleration-structures (AS) are referenced either through a VkAccelerationStructureKHR handle
    or their associated device-address.
    Aliasing or missing handles are (will be?) explicitly allowed, so the tracking has recently been reworked
    to focus on device-addresses.
    
    This PR fixes cases when the device address is not (yet) known and will query+update associated AS&buffer addresses adhoc, if necessary.
    Symptom were sometimes missing top-lvl AS during a replay when using -m rebind.
    
    VulkanAddressReplacer is now using replay- instead of capture-addresses for tracking internal resources.
    latter 'can' always be obtained, while former 'might' be unknown at replacement-time.
    
    Another minor fix is related to internally used host-memory, which is now using
    VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT unconditionally.
    previously we attempted to obtain memory using VK_MEMORY_PROPERTY_HOST_CACHED_BIT,
    which now showed issues on a previously untested config.
    
    Finally a minor fix to cleanup/lifetime of an internally used buffer (hashmap_control_block_bda_binary_)

--- framework/decode/vulkan_object_info.h
+++ framework/decode/vulkan_object_info.h
@@ -415,7 +415,7 @@ struct VulkanBufferInfo : public VulkanObjectInfo<VkBuffer>
     VkDeviceSize       size{ 0 };
     uint32_t           queue_family_index{ 0 };
 
-    // map acceleration-structure capture-addresses to existing (alias) AS-handles
+    // map acceleration-structure replay-addresses to existing (alias) AS-handles
     std::unordered_map<VkDeviceAddress, std::unordered_set<const VulkanAccelerationStructureKHRInfo*>>
         acceleration_structures;
 };
