76df7338c55d0d9123534988846dda7c5fc95ce7
Panagiotis Apostolou <panosa@lunarg.com>
2026-01-20 18:57:43 +0200

    Test for external trimming trigger

---
+++ test/test_apps/trigger-trimming/app.cpp
@@ -0,0 +1,417 @@
+/*
+** Copyright (c) 2025 LunarG, Inc.
+**
+** Permission is hereby granted, free of charge, to any person obtaining a
+** copy of this software and associated documentation files (the "Software"),
+** to deal in the Software without restriction, including without limitation
+** the rights to use, copy, modify, merge, publish, distribute, sublicense,
+** and/or sell copies of the Software, and to permit persons to whom the
+** Software is furnished to do so, subject to the following conditions:
+**
+** The above copyright notice and this permission notice shall be included in
+** all copies or substantial portions of the Software.
+**
+** THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+** IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+** FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+** AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+** LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+** FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+** DEALINGS IN THE SOFTWARE.
+*/
+
+#include "test_app_base.h"
+#include "util/callbacks.h"
+#include "util/logging.h"
+#include "util/platform.h"
+
+#include <dlfcn.h>
+#include <trigger_trimming_app.h>
+#include <vulkan/vulkan_core.h>
+
+GFXRECON_BEGIN_NAMESPACE(gfxrecon)
+GFXRECON_BEGIN_NAMESPACE(test_app)
+GFXRECON_BEGIN_NAMESPACE(trigger_trimming)
+
+// Structure passed down to callback mechanism
+struct FrameData
+{
+    size_t                  current_frame{ 0 };
+    GetCaptureModeGFXR_fp_t GetCaptureModeGFXR_fp{ nullptr };
+};
+
+// Callback for trim start
+static void EventStartCallback(void* data)
+{
+    VERIFY_CONDITION("data should not be NULL", data != nullptr);
+    const FrameData* frame_data = reinterpret_cast<const FrameData*>(data);
+    VERIFY_CONDITION("Wrong frame number", frame_data->current_frame == START_TRIMMING_FRAME);
+    VERIFY_CONDITION("Expected state is 3 (tracking & writing)", frame_data->GetCaptureModeGFXR_fp() == 3);
+}
+
+// Callback for trim end
+static void EventEndCallback(void* data)
+{
+    VERIFY_CONDITION("data should not be NULL", data != nullptr);
+    const FrameData* frame_data = reinterpret_cast<const FrameData*>(data);
+    VERIFY_CONDITION("Wrong frame number", frame_data->current_frame == STOP_TRIMMING_FRAME);
+    VERIFY_CONDITION("Expected state is 2 (tracking)", frame_data->GetCaptureModeGFXR_fp() == 2);
+}
+
+static FrameData frame_data_g;
+
+void App::create_render_pass()
+{
+    VkAttachmentDescription color_attachment = {};
+    color_attachment.format                  = init.swapchain.image_format;
+    color_attachment.samples                 = VK_SAMPLE_COUNT_1_BIT;
+    color_attachment.loadOp                  = VK_ATTACHMENT_LOAD_OP_CLEAR;
+    color_attachment.storeOp                 = VK_ATTACHMENT_STORE_OP_STORE;
+    color_attachment.stencilLoadOp           = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+    color_attachment.stencilStoreOp          = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+    color_attachment.initialLayout           = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+    color_attachment.finalLayout             = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+
+    VkAttachmentReference color_attachment_ref = {};
+    color_attachment_ref.attachment            = 0;
+    color_attachment_ref.layout                = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+
+    VkSubpassDescription subpass = {};
+    subpass.pipelineBindPoint    = VK_PIPELINE_BIND_POINT_GRAPHICS;
+    subpass.colorAttachmentCount = 1;
+    subpass.pColorAttachments    = &color_attachment_ref;
+
+    VkRenderPassCreateInfo render_pass_info = {};
+    render_pass_info.sType                  = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
+    render_pass_info.attachmentCount        = 1;
+    render_pass_info.pAttachments           = &color_attachment;
+    render_pass_info.subpassCount           = 1;
+    render_pass_info.pSubpasses             = &subpass;
+    render_pass_info.dependencyCount        = 0;
+
+    auto result = init.disp.createRenderPass(&render_pass_info, nullptr, &render_pass_);
+    VERIFY_VK_RESULT("failed to create render pass", result);
+}
+
+void App::create_framebuffers()
+{
+    framebuffers_.resize(init.swapchain_image_views.size());
+
+    for (size_t i = 0; i < init.swapchain_image_views.size(); i++)
+    {
+        VkImageView attachments[] = { init.swapchain_image_views[i] };
+
+        VkFramebufferCreateInfo framebuffer_info = {};
+        framebuffer_info.sType                   = VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO;
+        framebuffer_info.renderPass              = render_pass_;
+        framebuffer_info.attachmentCount         = 1;
+        framebuffer_info.pAttachments            = attachments;
+        framebuffer_info.width                   = init.swapchain.extent.width;
+        framebuffer_info.height                  = init.swapchain.extent.height;
+        framebuffer_info.layers                  = 1;
+
+        auto result = init.disp.createFramebuffer(&framebuffer_info, nullptr, &framebuffers_[i]);
+        VERIFY_VK_RESULT("failed to create framebuffer", result);
+    }
+}
+
+void App::recreate_swapchain()
+{
+    init.disp.deviceWaitIdle();
+
+    for (auto framebuffer : framebuffers_)
+    {
+        init.disp.destroyFramebuffer(framebuffer, nullptr);
+    }
+
+    TestAppBase::recreate_swapchain(false);
+
+    create_framebuffers();
+}
+
+#define IS_RUNNING(frame_num) frame_num < NUM_FRAMES;
+
+bool App::frame(const int frame_num)
+{
+    init.disp.waitForFences(1, &sync_.in_flight_fences[current_frame_], VK_TRUE, UINT64_MAX);
+
+    uint32_t image_index = 0;
+    VkResult result      = init.disp.acquireNextImageKHR(
+        init.swapchain, UINT64_MAX, sync_.available_semaphores[current_frame_], VK_NULL_HANDLE, &image_index);
+
+    if (result == VK_ERROR_OUT_OF_DATE_KHR)
+    {
+        recreate_swapchain();
+        return IS_RUNNING(frame_num);
+    }
+    else if (result != VK_SUCCESS && result != VK_SUBOPTIMAL_KHR)
+    {
+        throw gfxrecon::test::vulkan_exception("failed to acquire next image", result);
+    }
+
+    if (sync_.image_in_flight[image_index] != VK_NULL_HANDLE)
+    {
+        init.disp.waitForFences(1, &sync_.image_in_flight[image_index], VK_TRUE, UINT64_MAX);
+    }
+    sync_.image_in_flight[image_index] = sync_.in_flight_fences[current_frame_];
+
+    init.disp.resetCommandPool(command_pools_[current_frame_], 0);
+    VkCommandBufferAllocateInfo allocate_info = {};
+    allocate_info.sType                       = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+    allocate_info.commandBufferCount          = 1;
+    allocate_info.commandPool                 = command_pools_[current_frame_];
+    VkCommandBuffer command_buffer;
+    result = init.disp.allocateCommandBuffers(&allocate_info, &command_buffer);
+    VERIFY_VK_RESULT("failed to allocate command buffer", result);
+
+    {
+        VkCommandBufferBeginInfo begin_info = {};
+        begin_info.sType                    = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+        result                              = init.disp.beginCommandBuffer(command_buffer, &begin_info);
+        VERIFY_VK_RESULT("failed to create command buffer", result);
+
+        {
+            VkImageMemoryBarrier image_barrier        = {};
+            image_barrier.sType                       = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER;
+            image_barrier.image                       = init.swapchain_images[image_index];
+            image_barrier.oldLayout                   = VK_IMAGE_LAYOUT_UNDEFINED;
+            image_barrier.newLayout                   = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+            image_barrier.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+            image_barrier.subresourceRange.layerCount = VK_REMAINING_ARRAY_LAYERS;
+            image_barrier.subresourceRange.levelCount = VK_REMAINING_MIP_LEVELS;
+            image_barrier.srcAccessMask               = VK_ACCESS_NONE;
+            image_barrier.dstAccessMask               = VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT;
+            init.disp.cmdPipelineBarrier(command_buffer,
+                                         VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT,
+                                         VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT,
+                                         0,
+                                         0,
+                                         nullptr,
+                                         0,
+                                         nullptr,
+                                         1,
+                                         &image_barrier);
+        }
+
+        VkRenderPassBeginInfo render_pass_info = {};
+        render_pass_info.sType                 = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO;
+        render_pass_info.renderPass            = render_pass_;
+        render_pass_info.framebuffer           = framebuffers_[image_index];
+        render_pass_info.renderArea.offset     = { 0, 0 };
+        render_pass_info.renderArea.extent     = init.swapchain.extent;
+        VkClearValue clearColor{ { { 1.0f, 0.0f, 0.0f, 1.0f } } };
+        render_pass_info.clearValueCount = 1;
+        render_pass_info.pClearValues    = &clearColor;
+
+        VkViewport viewport = {};
+        viewport.x          = 0.0f;
+        viewport.y          = 0.0f;
+        viewport.width      = (float)init.swapchain.extent.width;
+        viewport.height     = (float)init.swapchain.extent.height;
+        viewport.minDepth   = 0.0f;
+        viewport.maxDepth   = 1.0f;
+
+        VkRect2D scissor = {};
+        scissor.offset   = { 0, 0 };
+        scissor.extent   = init.swapchain.extent;
+
+        init.disp.cmdSetViewport(command_buffer, 0, 1, &viewport);
+        init.disp.cmdSetScissor(command_buffer, 0, 1, &scissor);
+
+        init.disp.cmdBeginRenderPass(command_buffer, &render_pass_info, VK_SUBPASS_CONTENTS_INLINE);
+        init.disp.cmdEndRenderPass(command_buffer);
+
+        {
+            VkImageMemoryBarrier image_barrier        = {};
+            image_barrier.sType                       = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER;
+            image_barrier.image                       = init.swapchain_images[image_index];
+            image_barrier.oldLayout                   = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+            image_barrier.newLayout                   = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
+            image_barrier.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+            image_barrier.subresourceRange.layerCount = VK_REMAINING_ARRAY_LAYERS;
+            image_barrier.subresourceRange.levelCount = VK_REMAINING_MIP_LEVELS;
+            image_barrier.srcAccessMask               = VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT;
+            image_barrier.dstAccessMask               = VK_ACCESS_NONE;
+            init.disp.cmdPipelineBarrier(command_buffer,
+                                         VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT,
+                                         VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT,
+                                         0,
+                                         0,
+                                         nullptr,
+                                         0,
+                                         nullptr,
+                                         1,
+                                         &image_barrier);
+        }
+
+        result = init.disp.endCommandBuffer(command_buffer);
+        VERIFY_VK_RESULT("failed to end command buffer", result);
+    }
+
+    VkSubmitInfo submitInfo = {};
+    submitInfo.sType        = VK_STRUCTURE_TYPE_SUBMIT_INFO;
+
+    VkSemaphore          wait_semaphores[] = { sync_.available_semaphores[current_frame_] };
+    VkPipelineStageFlags wait_stages[]     = { VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT };
+    submitInfo.waitSemaphoreCount          = 1;
+    submitInfo.pWaitSemaphores             = wait_semaphores;
+    submitInfo.pWaitDstStageMask           = wait_stages;
+
+    submitInfo.commandBufferCount = 1;
+    submitInfo.pCommandBuffers    = &command_buffer;
+
+    VkSemaphore signal_semaphores[] = { sync_.finished_semaphore[current_frame_] };
+    submitInfo.signalSemaphoreCount = 1;
+    submitInfo.pSignalSemaphores    = signal_semaphores;
+
+    init.disp.resetFences(1, &sync_.in_flight_fences[current_frame_]);
+
+    result = init.disp.queueSubmit(graphics_queue_, 1, &submitInfo, sync_.in_flight_fences[current_frame_]);
+    VERIFY_VK_RESULT("failed to submit queue", result);
+
+    VkPresentInfoKHR present_info = {};
+    present_info.sType            = VK_STRUCTURE_TYPE_PRESENT_INFO_KHR;
+
+    present_info.waitSemaphoreCount = 1;
+    present_info.pWaitSemaphores    = signal_semaphores;
+
+    VkSwapchainKHR swapChains[] = { init.swapchain };
+    present_info.swapchainCount = 1;
+    present_info.pSwapchains    = swapChains;
+
+    present_info.pImageIndices = &image_index;
+
+    result = init.disp.queuePresentKHR(present_queue_, &present_info);
+    if (result == VK_ERROR_OUT_OF_DATE_KHR || result == VK_SUBOPTIMAL_KHR)
+    {
+        recreate_swapchain();
+        return frame_num >= NUM_FRAMES;
+    }
+    VERIFY_VK_RESULT("failed to present queue", result);
+
+    current_frame_ = (current_frame_ + 1) % MAX_FRAMES_IN_FLIGHT;
+
+    frame_data_g.current_frame = frame_num;
+
+    if (frame_num == START_TRIMMING_FRAME)
+    {
+        SetTrimmingStateGFXR_fp_(true);
+    }
+    else if (frame_num == STOP_TRIMMING_FRAME)
+    {
+        SetTrimmingStateGFXR_fp_(false);
+    }
+
+    return IS_RUNNING(frame_num);
+}
+
+void App::cleanup()
+{
+    for (size_t i = 0; i < MAX_FRAMES_IN_FLIGHT; i++)
+    {
+        init.disp.destroySemaphore(sync_.finished_semaphore[i], nullptr);
+        init.disp.destroySemaphore(sync_.available_semaphores[i], nullptr);
+        init.disp.destroyFence(sync_.in_flight_fences[i], nullptr);
+    }
+
+    for (auto command_pool : command_pools_)
+    {
+        init.disp.destroyCommandPool(command_pool, nullptr);
+    }
+
+    for (auto framebuffer : framebuffers_)
+    {
+        init.disp.destroyFramebuffer(framebuffer, nullptr);
+    }
+
+    init.disp.destroyPipeline(graphics_pipeline_, nullptr);
+    init.disp.destroyPipelineLayout(pipeline_layout_, nullptr);
+    init.disp.destroyRenderPass(render_pass_, nullptr);
+
+    if (layer_so_handle_ != nullptr)
+    {
+        util::platform::CloseLibrary(layer_so_handle_);
+    }
+}
+
+bool App::setup_function_pointers()
+{
+    // Open layer .so
+    layer_so_handle_ = util::platform::OpenLibrary(GFXRECON_LAYER_INSTALL_DIR "libVkLayer_gfxreconstruct.so");
+    if (layer_so_handle_ == nullptr)
+    {
+        GFXRECON_LOG_ERROR("dlopen failed %s", dlerror());
+        return false;
+    }
+
+    // Search for GFXRGetCaptureMode
+    frame_data_g.GetCaptureModeGFXR_fp = reinterpret_cast<GetCaptureModeGFXR_fp_t>(
+        util::platform::GetProcAddress(layer_so_handle_, "GFXRGetCaptureMode"));
+    if (frame_data_g.GetCaptureModeGFXR_fp == nullptr)
+    {
+        GFXRECON_LOG_ERROR("Failed to find \"GetCaptureMode\"")
+        util::platform::CloseLibrary(layer_so_handle_);
+        return false;
+    }
+
+    // Search for GFXRSetTrimmingState
+    SetTrimmingStateGFXR_fp_ = reinterpret_cast<SetTrimmingStateGFXR_fp_t>(
+        util::platform::GetProcAddress(layer_so_handle_, "GFXRSetTrimmingState"));
+    if (SetTrimmingStateGFXR_fp_ == nullptr)
+    {
+        GFXRECON_LOG_ERROR("Failed to find \"SetTrimmingState\"")
+        util::platform::CloseLibrary(layer_so_handle_);
+        return false;
+    }
+
+    // Search for appropriate callback setter
+    const gfxrecon::util::PFN_SetEventsCallbacks SetSignalTrimmingCallbacks_fp =
+        reinterpret_cast<const gfxrecon::util::PFN_SetEventsCallbacks>(
+            util::platform::GetProcAddress(layer_so_handle_, "SetSignalTrimmingCallbacks"));
+    if (SetSignalTrimmingCallbacks_fp == nullptr)
+    {
+        GFXRECON_LOG_ERROR("util::platform::GetProcAddress for \"SetSignalTrimmingCallbacks\" failed %s", dlerror());
+        util::platform::CloseLibrary(layer_so_handle_);
+        return false;
+    }
+
+    // Register callbacks
+    SetSignalTrimmingCallbacks_fp(EventStartCallback, EventEndCallback, reinterpret_cast<void*>(&frame_data_g));
+
+    return true;
+}
+
+void App::setup()
+{
+    auto graphics_queue = init.device.get_queue(gfxrecon::test::QueueType::graphics);
+    if (!graphics_queue.has_value())
+        throw std::runtime_error("could not get graphics queue");
+    graphics_queue_ = *graphics_queue;
+
+    auto present_queue = init.device.get_queue(gfxrecon::test::QueueType::present);
+    if (!present_queue.has_value())
+        throw std::runtime_error("could not get present queue");
+    present_queue_ = *present_queue;
+
+    if (!setup_function_pointers())
+    {
+        return;
+    }
+
+    create_render_pass();
+    create_framebuffers();
+
+    auto queue_family_index = init.device.get_queue_index(gfxrecon::test::QueueType::graphics);
+    if (!queue_family_index)
+        throw std::runtime_error("could not find graphics queue");
+    for (auto& command_pool : command_pools_)
+    {
+        command_pool = gfxrecon::test::create_command_pool(init.disp, *queue_family_index);
+    }
+
+    sync_ = gfxrecon::test::create_sync_objects(init.swapchain, init.disp, MAX_FRAMES_IN_FLIGHT);
+}
+
+GFXRECON_END_NAMESPACE(trigger_trimming)
+GFXRECON_END_NAMESPACE(test_app)
+GFXRECON_END_NAMESPACE(gfxrecon)
