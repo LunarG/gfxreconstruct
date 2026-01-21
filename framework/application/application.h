f717a481418819841ee798fceb833f30d6ed18f6
Antonio Caggiano <antonio@lunarg.com>
2026-01-21 11:19:36 +0100

    loop: Add frame loop support
    Add experimental frame looping by not advancing the preloaded frame
    after replaying a target frame.
    
    - Add `--loop-frame` and `--loop-count` replay options on desktop,
      Android, and in the launcher script.
    - Add `VulkanReplayFrameLoopConsumer` to enter loop mode at frame end,
      idle devices, not advancing to the next frame, and replay it.
    - Extend `PreloadFileProcessor` with `SetAdvanceToNextFrame()` to skip
      advancing to the next frame.
    - Allow Application to keep preloading while frame looping is active.
    
    Also, default `pause_frame` to uint32_t max. This is needed to avoid pausing
    the replay when frame looping is enabled for the first frame. In this case
    the frame counter is not incremented, therefore it is still 0 when it is
    checked against `pause_frame` (if default is 0) effectively matching its
    value which pauses replay. Setting pause_frame default to uint32_t max
    solves this edge case.
    
    Co-authored-by: Nick Driscoll <nick@lunarg.com>

--- framework/application/application.h
+++ framework/application/application.h
@@ -79,6 +79,8 @@ class Application final
 
     void SetFpsInfo(graphics::FpsInfo* fps_info);
 
+    void SetFrameLooping(bool active) { loop_frame_ = active; }
+
     bool InitializeWsiContext(const char* surfaceExtensionName, void* pPlatformSpecificData = nullptr);
 
 #if defined(WIN32)
@@ -98,6 +100,7 @@ class Application final
     decode::FileProcessor*                                       file_processor_;    ///< The FileProcessor object responsible for decoding and processing capture file data.
     bool                                                         running_;           ///< Indicates that the application is actively processing system events for playback.
     bool                                                         paused_;            ///< Indicates that the playback has been paused.  When paused the application will stop rendering, but will continue processing system events.
+    bool                                                         loop_frame_;      ///< Indicates that playback wishes to loop a certain frame
     uint32_t                                                     pause_frame_;       ///< The number for a frame that replay should pause after.
     std::unordered_map<std::string, std::unique_ptr<WsiContext>> wsi_contexts_;      ///< Loaded WSI contexts from CLI and VkInstanceCreateInfo
     std::string                                                  cli_wsi_extension_; ///< WSI extension selected on CLI, empty string if no CLI selection
