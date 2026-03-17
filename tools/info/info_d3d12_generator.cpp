58e1590a2cb76d2c5df2a4f19f106adc0f6f6584
Mark Young <marky@lunarg.com>
2026-03-17 12:06:18 -0400

    Rename to feature_module_registry

--- tools/info/info_d3d12_generator.cpp
+++ tools/info/info_d3d12_generator.cpp
@@ -25,14 +25,14 @@
 
 #include "info_d3d12_generator.h"
 
-#include "util/module_registry.h"
+#include "util/feature_module_registry.h"
 #include "util/to_string.h"
 
 GFXRECON_BEGIN_NAMESPACE(gfxrecon)
 GFXRECON_BEGIN_NAMESPACE(info)
 
-// Register this class as a module in a module registry
-GFXR_UTIL_REGISTER_MODULE(InfoD3d12Generator)
+// Register this class as a feature in a module registry
+GFXR_UTIL_REGISTER_FEATURE_CREATOR(InfoApiGenerator, InfoD3d12Generator)
 
 const char kEnumGpuIndices[] = "--enum-gpu-indices";
 
