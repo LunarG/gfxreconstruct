6bcc01404bdf58d645f976081d08772243756441
beau-lunarg <beau@lunarg.com>
2026-03-28 12:29:38 -0400

    Beau android reuse package (#2820)
    * Use --android-reuse-package cli option in VulkanTest
    
    * Update test ref
    
    * Update test.ref to merge commit

--- ci/runTestAndroid.sh
+++ ci/runTestAndroid.sh
@@ -5,4 +5,4 @@ echo creating Python virtual environment in $WORKSPACE/python-venv...
 python3 -m venv "$WORKSPACE/python-venv"
 "$WORKSPACE/python-venv/bin/python3" -m pip install --no-cache-dir -r VulkanTests/requirements.txt > "$WORKSPACE/python-venv.txt" 2>&1
 
-"$WORKSPACE/python-venv/bin/python3" VulkanTests/gfxrecontest.py --os AndroidTestOS --build-mode $BUILD_MODE $ANDROID_TEST_ARGS --suite "ci-gfxr-suites/$GFXRECON_TRACE_SUBDIR/$TEST_SUITE" --trace-dir "$GFXRECON_TRACE_DIR" --result-dir "$RESULTS_DIR"
+"$WORKSPACE/python-venv/bin/python3" VulkanTests/gfxrecontest.py --android-reuse-package --os AndroidTestOS --build-mode $BUILD_MODE $ANDROID_TEST_ARGS --suite "ci-gfxr-suites/$GFXRECON_TRACE_SUBDIR/$TEST_SUITE" --trace-dir "$GFXRECON_TRACE_DIR" --result-dir "$RESULTS_DIR"
