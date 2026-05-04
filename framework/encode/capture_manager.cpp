ee7f65c72c9eb5ffa7eb106b3754eb72e698f906
Locke Lin <locke@lunarg.com>
2026-05-04 16:18:49 -0600

    clang-format

--- framework/encode/capture_manager.cpp
+++ framework/encode/capture_manager.cpp
@@ -65,7 +65,7 @@ std::atomic<format::HandleId>              CommonCaptureManager::default_unique_
 uint64_t                                   CommonCaptureManager::default_unique_id_offset_ = 0;
 thread_local bool                          CommonCaptureManager::force_default_unique_id_  = false;
 thread_local std::vector<format::HandleId> CommonCaptureManager::unique_id_stack_;
-int64_t                                        CommonCaptureManager::avoid_api_call_lock_ = 0;
+int64_t                                    CommonCaptureManager::avoid_api_call_lock_ = 0;
 
 static std::mutex external_trim_trigger_mutex_g;
 static bool       externally_set_trimming_state_g          = false;
