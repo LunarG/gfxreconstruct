4adfce3611701d917277cb594907dca8e64f1e04
beau-lunarg <beau@lunarg.com>
2026-05-07 11:48:07 -0400

    ci: hardcode script runner label

--- ci/runJob.groovy
+++ ci/runJob.groovy
@@ -529,7 +529,6 @@ return [
     Win11Nvidia50XXLabel : 'Windows11-NVIDIA-50XX',
     WinAMDExtendedLabel: 'Windows-AMD-6800-tcwinamd2',
     WinNvidiaExtendedLabel: 'Windows-NVIDIA-2080-stable-exclusive',
-    ScriptLabel: 'linux',
 
     gfxrTestWindows: this.&gfxrTestWindows,
     gfxrTestLinux: this.&gfxrTestLinux,
