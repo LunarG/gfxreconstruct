acc6c8a071aa61b58b9be1ef77e38c4cc286a0a8
Beau Bennett <beau@lunarg.com>
2026-05-21 04:08:26 -0400

    Hardcode CI test bitness

--- ci/runTest.sh
+++ ci/runTest.sh
@@ -26,4 +26,4 @@ echo creating Python virtual environment in $WORKSPACE/python-venv...
 python3 -m venv "$WORKSPACE/python-venv"
 "$WORKSPACE/python-venv/bin/python3" -m pip install --no-cache-dir -r VulkanTests/requirements.txt > "$WORKSPACE/python-venv.txt" 2>&1
 
-"$WORKSPACE/python-venv/bin/python3" VulkanTests/gfxrecontest.py --no-build --build-mode ${BUILD_MODE} $LINUX_TEST_ARGS --suite "ci-gfxr-suites/$GFXRECON_TRACE_SUBDIR/$TEST_SUITE" --trace-dir "$GFXRECON_TRACE_DIR" --result-dir "$RESULTS_DIR"
+"$WORKSPACE/python-venv/bin/python3" VulkanTests/gfxrecontest.py --no-build --build-mode ${BUILD_MODE} --bits 64 $LINUX_TEST_ARGS --suite "ci-gfxr-suites/$GFXRECON_TRACE_SUBDIR/$TEST_SUITE" --trace-dir "$GFXRECON_TRACE_DIR" --result-dir "$RESULTS_DIR"
