7cad5c512fb084abb8a4fb2ccab5e6d518674fb7
Antonio Caggiano <antonio@lunarg.com>
2026-05-29 16:57:05 +0200

    decode: Pointer decoder GetSpan()

--- framework/decode/handle_pointer_decoder.h
+++ framework/decode/handle_pointer_decoder.h
@@ -59,6 +59,8 @@ class HandlePointerDecoder
 
     const format::HandleId* GetPointer() const { return decoder_.GetPointer(); }
 
+    std::span<const format::HandleId> GetSpan() const { return decoder_.GetSpan(); }
+
     void SetExternalMemory(T* data, size_t capacity)
     {
         assert(handle_data_ == nullptr);
