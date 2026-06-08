71f9dcd97f2bd02c794dea113309116f07e0d783
Nick Driscoll <nick@lunarg.com>
2026-06-08 12:20:31 -0400

    Gather all dangling descriptor sets in Process_vkFreeDescriptorSets, not just one

--- framework/decode/vulkan_replay_frame_loop_consumer.cpp
+++ framework/decode/vulkan_replay_frame_loop_consumer.cpp
@@ -170,10 +170,6 @@ void VulkanReplayFrameLoopConsumer::Process_vkResetDescriptorPool(const ApiCallI
     }
 
     VulkanReplayConsumer::Process_vkResetDescriptorPool(call_info, returnValue, device, descriptorPool, flags);
-
-    // For pools that contain dangling descriptor sets, this code will only be reached once,
-    // during the final iteration of the loop range.
-    RemovePoolDanglingCreateDescriptors(descriptorPool);
 }
 
 void VulkanReplayFrameLoopConsumer::Process_vkAllocateDescriptorSets(
