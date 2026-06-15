40fa182f0aaf5baa56fb35ff474349f3016f3700
wzchu-lunarg <william.chu@lunarg.com>
2026-06-15 09:52:39 -0600

    Change order of deferred operation pipeline wrapper setters (#3017)
    * Add null check and set to associated acc_wrappers
    
    This is due to a case where we are destroying multiple buffers. Each time we attempt to destroy a buffer, we iterate through all acceleration structures to find ones that are associated to that buffer's wrapper for tracking. After finding these associated structures, we free the buffer wrapper.
    
    However, if we destroy multiple of these acceleration structure's buffers, on second pass, since we `VisitWrappers` for all the acceleration structures, we hit one where `acc_wrapper->buffer` is already deleted and is a dangling pointer, and therefore results in a read violation.
    
    Instead of leaving `acc_wrapper->buffer` as dangling, set is as 'nullptr' which we can also later use to early exit out of processing this acceleration structure.
    
    * Change order of deferred operation pipeline wrapper setters
    
    Motivated by issue when capturing "Doom: The Dark Ages" with trimming. This issue is due to writing a `vkGetRayTracingShaderGroupHandlesKHR` command with `data_size = 0`, which not only violates the Vulkan spec, but also causes an assertion error during replay in `VulkanReplayConsumerBase::OverrideGetRayTracingShaderGroupHandlesKHR` for having `pData` be null.
    
    The writer's `data_size` is based off of the pipeline wrapper's `wrapper->num_shader_group_handles`, which happened to be 0. This was due to it not being set in `VulkanCaptureManager::DeferredOperationPostProcess` and defaulting to 0. However, why are we setting it in a `rayTracingPipelineShaderGroupHandleCaptureReplay` feature guard?
    
    Move the set for `num_shader_group_handles` to be outside the feature guard, which also better mirrors the logic in `OverrideCreateRayTracingPipelinesKHR`.
    
    Also remove the redundant `if (deferred_operation_wrapper)` guard.

--- framework/encode/vulkan_capture_manager.cpp
+++ framework/encode/vulkan_capture_manager.cpp
@@ -1793,24 +1793,20 @@ void VulkanCaptureManager::DeferredOperationPostProcess(VkDevice               d
             auto pipeline_wrapper = vulkan_wrappers::GetWrapper<vulkan_wrappers::PipelineWrapper>(
                 deferred_operation_wrapper->pPipelines[i]);
 
-            if (deferred_operation_wrapper)
-            {
-                // We need to set device_id here because some hardware may not have the feature
-                // rayTracingPipelineShaderGroupHandleCaptureReplay so the device_id cannot be set by
-                // VulkanStateTracker::TrackRayTracingShaderGroupHandles
-                pipeline_wrapper->device_id = vulkan_wrappers::GetWrappedId<vulkan_wrappers::DeviceWrapper>(device);
-                pipeline_wrapper->deferred_operation.handle_id         = deferred_operation_wrapper->handle_id;
-                pipeline_wrapper->deferred_operation.create_call_id    = deferred_operation_wrapper->create_call_id;
-                pipeline_wrapper->deferred_operation.create_parameters = deferred_operation_wrapper->create_parameters;
-            }
+            // We need to set device_id here because some hardware may not have the feature
+            // rayTracingPipelineShaderGroupHandleCaptureReplay so the device_id cannot be set by
+            // VulkanStateTracker::TrackRayTracingShaderGroupHandles
+            pipeline_wrapper->device_id = vulkan_wrappers::GetWrappedId<vulkan_wrappers::DeviceWrapper>(device);
+            pipeline_wrapper->num_shader_group_handles     = deferred_operation_wrapper->create_infos[i].groupCount;
+            pipeline_wrapper->deferred_operation.handle_id = deferred_operation_wrapper->handle_id;
+            pipeline_wrapper->deferred_operation.create_call_id    = deferred_operation_wrapper->create_call_id;
+            pipeline_wrapper->deferred_operation.create_parameters = deferred_operation_wrapper->create_parameters;
 
             if (device_wrapper->property_feature_info.feature_rayTracingPipelineShaderGroupHandleCaptureReplay)
             {
                 const uint32_t data_size =
                     device_wrapper->property_feature_info.property_shaderGroupHandleCaptureReplaySize *
                     deferred_operation_wrapper->create_infos[i].groupCount;
-
-                pipeline_wrapper->num_shader_group_handles = deferred_operation_wrapper->create_infos[i].groupCount;
 
                 std::vector<uint8_t> data(data_size);
                 result = device_table->GetRayTracingCaptureReplayShaderGroupHandlesKHR(
