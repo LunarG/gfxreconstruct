f07c1ff903bcd7680c10d73a080de6d1434391a4
Oleh Kuznetsov <okuznetsov@google.com>
2026-06-24 13:02:02 +0100

    Create gfxrecon_android_benchmark_plugin
    plugin uses atrace to record boundaries of frame and queue submits

--- framework/plugin/sample/replay_event_plugin.cpp
+++ framework/plugin/sample/replay_event_plugin.cpp
@@ -24,40 +24,6 @@
 
 #include <util/logging.h>
 
-#include <chrono>
-#include <sstream>
-#include <string>
-#include <thread>
-#include <vector>
-
-#ifdef __ANDROID__
-#include <android/trace.h>
-#endif
-
-namespace
-{
-void TraceBeginSection(const char* name)
-{
-#ifdef __ANDROID__
-    ATrace_beginSection(name);
-#endif
-}
-
-void TraceEndSection()
-{
-#ifdef __ANDROID__
-    ATrace_endSection();
-#endif
-}
-} // namespace
-
-struct SampleReplayPlugin
-{
-    GfxrReplayPluginV1 base;
-    int                sleep_around_gpu_frame_ms   = 0;
-    bool               first_queue_submit_in_frame = true;
-};
-
 static void destroy(GfxrReplayPluginV1* self)
 {
     if (self == NULL)
@@ -67,7 +33,6 @@ static void destroy(GfxrReplayPluginV1* self)
     }
 
     GFXRECON_LOG_INFO("Destroying plugin");
-    delete reinterpret_cast<SampleReplayPlugin*>(self);
 }
 
 static GfxrReplayPluginResult on_event(GfxrReplayPluginV1* self, const GfxrReplayEventHeader* event)
@@ -85,43 +50,15 @@ static GfxrReplayPluginResult on_event(GfxrReplayPluginV1* self, const GfxrRepla
     }
 
     GFXRECON_LOG_INFO("Received event type %u at timestamp %llu ns", event->type, event->timestamp_ns);
-
-    SampleReplayPlugin* plugin        = reinterpret_cast<SampleReplayPlugin*>(self);
-    int                 half_sleep_ms = plugin->sleep_around_gpu_frame_ms / 2;
-
-    switch (event->type)
-    {
-        case GFXR_REPLAY_EVENT_QUEUE_SUBMIT_BEGIN:
-            if (plugin->first_queue_submit_in_frame)
-            {
-                if (half_sleep_ms > 0)
-                    std::this_thread::sleep_for(std::chrono::milliseconds(half_sleep_ms));
-                TraceBeginSection("GFXRFrame");
-                if (half_sleep_ms > 0)
-                    std::this_thread::sleep_for(std::chrono::milliseconds(half_sleep_ms));
-                plugin->first_queue_submit_in_frame = false;
-            }
-            TraceBeginSection("GFXRQueueSubmit");
-            break;
-        case GFXR_REPLAY_EVENT_QUEUE_SUBMIT_END:
-            TraceEndSection();
-            break;
-        case GFXR_REPLAY_EVENT_FRAME_BEGIN:
-            plugin->first_queue_submit_in_frame = true;
-            break;
-        case GFXR_REPLAY_EVENT_FRAME_END:
-            if (half_sleep_ms > 0)
-                std::this_thread::sleep_for(std::chrono::milliseconds(half_sleep_ms));
-            TraceEndSection();
-            if (half_sleep_ms > 0)
-                std::this_thread::sleep_for(std::chrono::milliseconds(half_sleep_ms));
-            break;
-        default:
-            break;
-    }
-
     return GFXR_REPLAY_PLUGIN_RESULT_OK;
 }
+
+static GfxrReplayPluginV1 plugin = {
+    GFXR_REPLAY_PLUGIN_ABI_VERSION,
+    sizeof(GfxrReplayPluginV1),
+    destroy,
+    on_event,
+};
 
 GFXR_REPLAY_PLUGIN_EXPORT GfxrReplayPluginV1* gfxrCreateReplayPluginV1(const GfxrReplayPluginCreateInfo* create_info)
 {
@@ -147,55 +84,5 @@ GFXR_REPLAY_PLUGIN_EXPORT GfxrReplayPluginV1* gfxrCreateReplayPluginV1(const Gfx
     GFXRECON_LOG_INFO("Creating sample plugin with params: %s",
                       create_info->plugin_params ? create_info->plugin_params : "null");
 
-    SampleReplayPlugin* plugin = new SampleReplayPlugin;
-    plugin->base.abi_version   = GFXR_REPLAY_PLUGIN_ABI_VERSION;
-    plugin->base.struct_size   = sizeof(GfxrReplayPluginV1);
-    plugin->base.destroy       = destroy;
-    plugin->base.on_event      = on_event;
-
-    if (create_info->plugin_params != nullptr)
-    {
-        std::string              params_str(create_info->plugin_params);
-        std::istringstream       iss(params_str);
-        std::string              token;
-        std::vector<std::string> args;
-
-        while (iss >> token)
-        {
-            args.push_back(token);
-        }
-
-        for (size_t i = 0; i < args.size(); ++i)
-        {
-            if (args[i].find("sleep-around-gpu-frame-ms=") == 0)
-            {
-                try
-                {
-                    plugin->sleep_around_gpu_frame_ms = std::stoi(args[i].substr(26));
-                }
-                catch (...)
-                {
-                    GFXRECON_LOG_ERROR("Invalid value for sleep-around-gpu-frame-ms: %s", args[i].c_str());
-                }
-            }
-            else if (args[i] == "sleep-around-gpu-frame-ms" && i + 1 < args.size())
-            {
-                try
-                {
-                    plugin->sleep_around_gpu_frame_ms = std::stoi(args[i + 1]);
-                }
-                catch (...)
-                {
-                    GFXRECON_LOG_ERROR("Invalid value for sleep-around-gpu-frame-ms: %s", args[i + 1].c_str());
-                }
-                i++; // Skip the value argument
-            }
-            else
-            {
-                GFXRECON_LOG_WARNING("Unrecognized plugin parameter: %s", args[i].c_str());
-            }
-        }
-    }
-
-    return &plugin->base;
+    return &plugin;
 }
