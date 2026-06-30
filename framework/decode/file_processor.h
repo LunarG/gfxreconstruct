d9481f02ce0b02850d7847d5cf6a44e1ce4a7cb3
Oleh Kuznetsov <okuznetsov@google.com>
2026-06-30 12:03:05 +0100

    [frame looping] Enforce loop frame boundary termination in DispatchVisitor

--- framework/decode/file_processor.h
+++ framework/decode/file_processor.h
@@ -101,6 +101,7 @@ class FileProcessor
     // Returns true if there are more frames to process, false if all frames have been processed or an error has
     // occurred.  Use GetErrorState() to determine error condition.
     virtual bool ProcessNextFrame();
+    virtual bool IsLoopReplay() const { return false; }
 
     // Returns false if processing failed.  Use GetErrorState() to determine error condition for failure case.
     bool ProcessAllFrames();
