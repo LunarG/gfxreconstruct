5de0f63c437f5198d4b68b8e0d88e2383ec3cc8c
Panagiotis Apostolou <panosa@lunarg.com>
2026-07-01 10:18:51 +0300

    Add a unit test for SubmitInfo2Translator

--- framework/decode/test/main.cpp
+++ framework/decode/test/main.cpp
@@ -31,6 +31,8 @@
 #include "decode/common_object_info_table.h"
 #include "format/format.h"
 #include "format/format_util.h"
+#include "graphics/vulkan_submit_info_util.h"
+#include "graphics/vulkan_struct_get_pnext.h"
 
 #include "decode/block_parser.h"
 
@@ -161,4 +163,87 @@ TEST_CASE("BlockParser basic usage", "[wrapper]")
     // this should trigger some error
     block_parser.HandleBlockReadError(gfxrecon::decode::BlockIOError::kErrorReadingBlockData, "fatal fake error");
     REQUIRE(err_triggered);
+}
+
+TEST_CASE("SubmitInfo2Translator widens VkSubmitInfo into VkSubmitInfo2", "[]")
+{
+    // Handles are opaque here; fabricate distinct non-null values. They only need to compare equal.
+    const VkSemaphore     wait_semaphores[2]   = { gfxrecon::format::FromHandleId<VkSemaphore>(0x11),
+                                                   gfxrecon::format::FromHandleId<VkSemaphore>(0x12) };
+    const VkSemaphore     signal_semaphores[1] = { gfxrecon::format::FromHandleId<VkSemaphore>(0x21) };
+    const VkCommandBuffer command_buffers[1]   = { gfxrecon::format::FromHandleId<VkCommandBuffer>(0x31) };
+
+    const VkPipelineStageFlags wait_stages[2] = { VK_PIPELINE_STAGE_VERTEX_SHADER_BIT, VK_PIPELINE_STAGE_TRANSFER_BIT };
+
+    // Values carried by the three pNext structures that must be translated into VkSubmitInfo2 fields and removed from
+    // the chain.
+    const uint64_t wait_values[2]           = { 7, 8 };
+    const uint64_t signal_values[1]         = { 42 };
+    const uint32_t wait_device_indices[2]   = { 1, 0 };
+    const uint32_t signal_device_indices[1] = { 1 };
+    const uint32_t command_buffer_masks[1]  = { 0x2 };
+
+    VkDeviceGroupSubmitInfo       device_group{ VK_STRUCTURE_TYPE_DEVICE_GROUP_SUBMIT_INFO,
+                                          nullptr,
+                                          2,
+                                          wait_device_indices,
+                                          1,
+                                          command_buffer_masks,
+                                          1,
+                                          signal_device_indices };
+    VkProtectedSubmitInfo         protected_info{ VK_STRUCTURE_TYPE_PROTECTED_SUBMIT_INFO, &device_group, VK_TRUE };
+    VkTimelineSemaphoreSubmitInfo timeline{
+        VK_STRUCTURE_TYPE_TIMELINE_SEMAPHORE_SUBMIT_INFO, &protected_info, 2, wait_values, 1, signal_values
+    };
+
+    VkSubmitInfo info{};
+    info.sType                = VK_STRUCTURE_TYPE_SUBMIT_INFO;
+    info.pNext                = &timeline;
+    info.waitSemaphoreCount   = 2;
+    info.pWaitSemaphores      = wait_semaphores;
+    info.pWaitDstStageMask    = wait_stages;
+    info.commandBufferCount   = 1;
+    info.pCommandBuffers      = command_buffers;
+    info.signalSemaphoreCount = 1;
+    info.pSignalSemaphores    = signal_semaphores;
+
+    std::vector<VkSubmitInfo>                 infos_v1{ info };
+    gfxrecon::graphics::SubmitInfo2Translator translator(infos_v1);
+    const auto&                               infos_v2 = translator.GetSubmitInfos2();
+
+    REQUIRE(infos_v1.size() == infos_v2.size());
+
+    const VkSubmitInfo2& s = infos_v2[0];
+    REQUIRE(s.sType == VK_STRUCTURE_TYPE_SUBMIT_INFO_2);
+
+    // VkProtectedSubmitInfo::protectedSubmit becomes a submit flag.
+    REQUIRE((s.flags & VK_SUBMIT_PROTECTED_BIT) != 0);
+
+    // Wait semaphores carry the handle, the widened wait-stage mask, the timeline value and the device index.
+    REQUIRE(s.waitSemaphoreInfoCount == 2);
+    REQUIRE(s.pWaitSemaphoreInfos[0].semaphore == wait_semaphores[0]);
+    REQUIRE(s.pWaitSemaphoreInfos[0].stageMask == static_cast<VkPipelineStageFlags2>(wait_stages[0]));
+    REQUIRE(s.pWaitSemaphoreInfos[0].value == wait_values[0]);
+    REQUIRE(s.pWaitSemaphoreInfos[0].deviceIndex == wait_device_indices[0]);
+    REQUIRE(s.pWaitSemaphoreInfos[1].semaphore == wait_semaphores[1]);
+    REQUIRE(s.pWaitSemaphoreInfos[1].stageMask == static_cast<VkPipelineStageFlags2>(wait_stages[1]));
+    REQUIRE(s.pWaitSemaphoreInfos[1].value == wait_values[1]);
+    REQUIRE(s.pWaitSemaphoreInfos[1].deviceIndex == wait_device_indices[1]);
+
+    // Command buffers carry the handle and the device mask.
+    REQUIRE(s.commandBufferInfoCount == 1);
+    REQUIRE(s.pCommandBufferInfos[0].commandBuffer == command_buffers[0]);
+    REQUIRE(s.pCommandBufferInfos[0].deviceMask == command_buffer_masks[0]);
+
+    // Signal semaphores signal at ALL_COMMANDS and carry the timeline value and device index.
+    REQUIRE(s.signalSemaphoreInfoCount == 1);
+    REQUIRE(s.pSignalSemaphoreInfos[0].semaphore == signal_semaphores[0]);
+    REQUIRE(s.pSignalSemaphoreInfos[0].stageMask == VK_PIPELINE_STAGE_2_ALL_COMMANDS_BIT);
+    REQUIRE(s.pSignalSemaphoreInfos[0].value == signal_values[0]);
+    REQUIRE(s.pSignalSemaphoreInfos[0].deviceIndex == signal_device_indices[0]);
+
+    // All three translated structures must be stripped from the VkSubmitInfo2 pNext-chain.
+    REQUIRE(gfxrecon::graphics::vulkan_struct_get_pnext<VkTimelineSemaphoreSubmitInfo>(&s) == nullptr);
+    REQUIRE(gfxrecon::graphics::vulkan_struct_get_pnext<VkProtectedSubmitInfo>(&s) == nullptr);
+    REQUIRE(gfxrecon::graphics::vulkan_struct_get_pnext<VkDeviceGroupSubmitInfo>(&s) == nullptr);
 }
