8c74f993950d3944ec56d6143ca66c001de4ed73
Panagiotis Apostolou <panosa@lunarg.com>
2026-07-21 12:51:20 +0300

    storage descriptors should be read | write

--- framework/encode/vulkan_handle_wrappers.h
+++ framework/encode/vulkan_handle_wrappers.h
@@ -572,7 +572,8 @@ struct CommandBufferWrapper : public HandleWrapper<VkCommandBuffer>
 
         void SetAccessType(ResourceAccessType access)
         {
-            GFXRECON_ASSERT(access == ResourceAccessType::kRead || access == ResourceAccessType::kWrite);
+            GFXRECON_ASSERT(access == ResourceAccessType::kRead || access == ResourceAccessType::kWrite ||
+                            access == (ResourceAccessType::kRead | ResourceAccessType::kWrite));
             access_type |= access;
         }
 
